e65a30e287e28daf76bb870dad1b332d6f14471d
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-16 22:26:43 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,29 @@
+const instanceObject = {
+  env: {
+    log
+  }
+}
+
+function getStringFromMemory(offset, length) {
+  return Array
+    .from(new Uint8Array(memory.buffer, offset, length))
+    .map(c => String.fromCharCode(c))
+    .join('')
+}
+
+function render(html) {
+  const app = document.getElementById("app")
+  app.innerHTML = html
+}
+
+function log(offset, length) {
+  const str = getStringFromMemory(offset, length)
+  render(str)
+}
+
+WebAssembly.instantiateStreaming(fetch('../out/main.wasm'), instanceObject).then(
+  obj => {
+    memory = obj.instance.exports.memory
+    obj.instance.exports.hello_world()
+  }
+)
