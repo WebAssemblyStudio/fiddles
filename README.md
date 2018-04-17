6ed5beb659a5d09c70997bdd2f8810b6e620a8e9
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-17 20:30:16 +0000

    WebAssembly Studio fiddle

---
+++ README.md
@@ -0,0 +1,10 @@
+# `console.log`
+
+This is an example of using the `#[wasm_bindgen]` macro to access the `console.log` function in JS.
+
+Hit "Build" and then "Run" up top and you should see some console logs in "Output" below.
+
+Be sure to check out this [example on GitHub][gh] or the [`hello_world`][hello] example which should have some more comments and such.
+
+[gh]: https://github.com/rustwasm/wasm-bindgen/tree/master/examples/console_log
+[hello]: https://webassembly.studio/?f=gzubao6tg3
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_many(a: &str, b: &str);
+}
+
+#[wasm_bindgen]
+pub fn run() {
+    log("Hello from Rust!");
+    log_u32(42);
+    log_many("Logging", "many values!");
+}
