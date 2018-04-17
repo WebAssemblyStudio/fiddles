8d7ad462c3ea6df7c21d243a7ec3e4dfa6b3dd19
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-17 22:20:38 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,69 @@
+import 'allocator/buddy';
+
+export function allocate(size: usize): usize {
+    return allocate_memory(size);
+}
+
+export function deallocate(ptr: usize): void {
+    free_memory(ptr);
+}
+
+export function add(x: i32, y: i32): i32 {
+    return x + y;
+}
+
+export function make_string(count: usize): string {
+    return 'na '.repeat(count) + 'batman!';
+}
+
+export function str_length(str: string): usize {
+    return str.length;
+}
+
+class Foo {
+    a: u32;
+    b: u8;
+    c: u16;
+
+    constructor(a: u32, b: u8, c: u16) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    add(): usize {
+        return this.a + this.b + this.c;
+    }
+}
+
+export function make_foo(): Foo {
+    return new Foo(1111, 222, 3333);
+}
+
+export function add_foo(foo: Foo): usize {
+    let result = foo.add();
+    free_memory(changetype<usize>(foo));
+
+    return result;
+}
+
+export function sum(arr: ArrayBuffer): usize {
+    // need to make a Uint32Array from the buffer
+    let view_ptr = allocate_memory(12);
+    store<usize>(view_ptr, changetype<usize>(arr)); // buffer
+    store<usize>(view_ptr + 4, 0);                  // offset
+    store<usize>(view_ptr + 8, arr.byteLength);     // byteLength
+
+    let view = changetype<Uint32Array>(view_ptr);
+    let result = 0;
+
+    for (let i = 0; i < view.length; i++) {
+        result += view[i];
+    }
+
+    return result;
+}
+
+export function throw_error(): void {
+    throw new Error();
+}
---
+++ src/main.js
@@ -0,0 +1,54 @@
+const Foo = new ffi.Struct({
+  a: 'u32',
+  b: 'u8',
+  c: 'u16',
+});
+
+
+const lib = new ffi.Wrapper({
+  // basic numbers
+  add:         ['number', ['number', 'number']],
+  // strings
+  make_string: ['string', ['number']],
+  str_length:  ['number', ['string']],
+  // classes
+  make_foo:    [Foo],
+  add_foo:     ['number', [Foo]],
+  // arrays
+  sum:         ['number', ['array']],
+}, {
+  // options
+  dialect: 'assemblyscript'
+});
+
+
+lib.fetch('/out/main.wasm').then(() => {
+  console.log('basic numbers, lib.add(1, 2): ' + lib.add(1, 2));
+
+  console.log('make_string: ' + lib.make_string(7));
+  console.log('str_length of `9 letters`: ' + lib.str_length('9 letters'));
+
+  const foo = lib.make_foo();
+  console.log(`foo: ${foo}`);
+
+  foo.a = 1;
+  foo.b = 2;
+  foo.c = 3;
+  console.log(`foo mutated: ${foo}`);
+  console.log("adding foo's fields: " + lib.add_foo(foo));
+
+  const foo2 = new Foo({
+    a: 4,
+    b: 5,
+    c: 6,
+  });
+
+  console.log(`made new class: ${foo2}`);
+  console.log("adding foo2's fields: " + lib.add_foo(foo2));
+
+  const arr = new Uint32Array([1, 2, 3, 4, 5]);
+  console.log(`Sum of array (${arr}): ` + lib.sum(arr));
+
+  console.log('Throwing error (check console):');
+  lib.exports.throw_error();
+});
