e89e4bd400d895e33d95009f41ae5ac043c40576
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-18 06:10:57 +0000

    WebAssembly Studio fiddle

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::ffi::CString;
+
+extern {
+  fn update_html(_:i32, _:i32);
+}
+
+unsafe fn update(string: &str) {
+  let length = string.len() as i32;
+  let cstring = CString::new(string).unwrap();
+  let offset = cstring.into_raw() as i32;
+  update_html(offset, length);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn init() {
+  update("Hello world ");
+}
