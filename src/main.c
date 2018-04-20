680b7855c058735628cb7398474e8e299e359b61
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-20 02:20:43 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/uio.h>
+
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main() {
+
+  int z = 111;
+
+  printf("Hello World -- Dum %i\n",z);
+
+  for (z = 0 ; z < 100 ; z++)  {
+    printf("Item %i\n", z);
+  }
+
+  //printf("asdf\n");
+}
+
+/* External function that is implemented in JavaScript. */
+extern void putc_js(char c);
+
+/* Basic implementation of the writev sys call. */ 
+WASM_EXPORT
+size_t writev_c(int fd, const struct iovec *iov, int iovcnt) {
+  size_t cnt = 0;
+  for (int i = 0; i < iovcnt; i++) {
+    for (int j = 0; j < iov[i].iov_len; j++) {
+      putc_js(((char *)iov[i].iov_base)[j]);
+    }
+    cnt += iov[i].iov_len;
+  }
+  return cnt;
+}
