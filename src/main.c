e44f901f14ca68a0f09e2762da8b2a6d51a977b6
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-21 08:35:15 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main () {
+
+   int a;
+	
+   /* for loop execution */
+   for( a = 10; a < 20; a = a + 1 ){
+      printf("value of a: %d\n", a);
+   }
+ 
+   return 0;
+}
