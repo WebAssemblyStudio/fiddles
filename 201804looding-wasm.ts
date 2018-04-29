17424f2cb6bb71608b2451a055af8784ac5760ee
WebAssemblyStudio <fiddles@webassembly.studio>
2018-04-29 20:18:29 +0000

    WebAssembly Studio fiddle

---
+++ 201804looding-wasm.ts
@@ -0,0 +1,7 @@
+// This is our recommended way of loading WebAssembly.
+(async () => {
+  const fetchPromise = fetch('fibonacci.wasm');
+  const { instance } = await WebAssembly.instantiateStreaming(fetchPromise);
+  const result = instance.exports.fibonacci(42);
+  console.log(result);
+})();
