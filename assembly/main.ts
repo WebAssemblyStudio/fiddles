b8137d463a90cae2cf6c85cbf686959f0d86de7e
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-08 15:30:26 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,73 @@
+
+export function test(t:i32):void {
+
+	var s00:i64;
+	var s01:i64;
+	var s02:i64;
+	var s03:i64;
+	var s04:i64;
+	var s05:i64;
+	var s06:i64;
+	var s07:i64;
+	var s08:i64;
+	var s09:i64;
+	var s10:i64;
+	var s11:i64;
+	var s12:i64;
+	var s13:i64;
+	var s14:i64;
+	var s15:i64;
+	var s16:i64;
+	var s17:i64;
+	var s18:i64;
+	var s19:i64;
+	var s20:i64;
+	var s21:i64;
+	var s22:i64;
+	var s23:i64;
+	var s24:i64;
+
+	var	b00:i64;
+	var b01:i64;
+	var b02:i64;
+	var b03:i64;
+	var b04:i64;
+	var b05:i64;
+	var b06:i64;
+	var b07:i64;
+	var b08:i64;
+	var b09:i64;
+	var b10:i64;
+	var b11:i64;
+	var b12:i64;
+	var b13:i64;
+	var b14:i64;
+	var b15:i64;
+	var b16:i64;
+	var b17:i64;
+	var b18:i64;
+	var b19:i64;
+	var b20:i64;
+	var b21:i64;
+	var b22:i64;
+	var b23:i64;
+	var b24:i64;
+
+	var ut:i64;
+
+	var a0:u64;
+	var a1:u64;
+	var b0:u64;
+	var b1:u64;
+	var c0:u64;
+	var c1:u64;
+
+	var a:u64;
+	var b:u64;
+	var c:u64;
+	var d:u64;
+	var ad:u64;
+	var bd:u64;
+	var adbc:u64;
+
+}
