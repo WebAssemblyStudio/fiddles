ed722f6c3014ef25705d6d3d86bf6feda6987ede
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-14 06:44:54 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,43 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+
+export function sub(x: i32, y: i32): i32 {
+  return x - y;
+}
+
+export function mul(x: i32, y: i32): i32 {
+  return x * y;
+}
+
+export function div(x: i32, y: i32): i32 {
+  return x / y;
+}
+
+export function pow(x: i32, y: i32): i32 {
+  return x / y;
+}
+
+export function log(x: i32, y: i32): i32 {
+  return x / y;
+}
+
+export function sqr(x: i32, y: i32): i32 {
+  return x / y;
+}
+
+export function sin(x: i32, y: i32): i32 {
+  return x / y;
+}
+
+export function cos(x: i32, y: i32): i32 {
+  return x / y;
+}
+
+export function tan(x: i32, y: i32): i32 {
+  return x / y;
+}
