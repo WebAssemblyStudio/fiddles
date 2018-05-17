5df2580698a54c1f447aa9a5805c6ae9fdab322a
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-17 20:49:39 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const { greet, get_string } = wasm_bindgen;
+
+function runApp() {
+  greet('World');
+  let strFromWasm = get_string();
+  console.log(strFromWasm);
+}
+
+// Load and instantiate the wasm file, and we specify the source of the wasm
+// file here. Once the returned promise is resolved we're ready to go and
+// use our imports.
+wasm_bindgen('../out/main_bg.wasm').then(runApp);
