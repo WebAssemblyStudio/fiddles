b9d79f6dbb520563f7ba31464e882450162d010c
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-21 15:03:17 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+#include <math.h>
+
+int mod (int a, int b)
+{
+   if(b < 0) //you can check for b == 0 separately and do what you want
+     return mod(a, -b);   
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
+
+WASM_EXPORT
+int little_fermat(number) {
+  if (mod(pow(2, number), number) == 2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+WASM_EXPORT
+int isprime(number) {
+  for (int a = 2; a < sqrt(number); a = a + 1) {
+    if (mod(number, a) == 0) {
+      return 0;
+    }
+  }
+  return 1;
+}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  document.getElementById("container").innerText = "Fermat : " + instance.exports.little_fermat(5);
+  document.getElementById("container").innerText = "Legacy : " + instance.exports.isprime(5);
+});
