737a5cdd892626b1c36381d0e2fc8e76cb813a5a
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-21 14:52:08 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,30 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+#include <math.h>
+
+int mod (int a, int b)
+{
+   if(b < 0) //you can check for b == 0 separately and do what you want
+     return mod(a, -b);   
+   int ret = a % b;
+   if(ret < 0)
+     ret+=b;
+   return ret;
+}
+
+WASM_EXPORT
+int little_fermat(number) {
+  if (mod(pow(2, number), number) == 2) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int isprime(number) {
+  for (int a = 2; a < sqrt(number); a = a + 1) {
+    if (number % a == 0 && number != a) {
+      return 1;
+    }
+  }
+  return 0;
+}
