6154561d443f9e1ad13ef79022fb2e0eed6ebc1d
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-22 14:52:30 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,4 @@
+
+export function div16_internal(x: i16, y: i16): i16 {
+  return x / y + x / y;
+}
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require("gulp");
+
+gulp.task("build", callback => {
+  const asc = require("assemblyscript/bin/asc");
+  asc.main([
+    "main.ts",
+    "--baseDir", "assembly",
+    "--binaryFile", "../out/main.wasm",
+    "--sourceMap",
+    "--measure",
+    "--noDebug",
+    "--noAssert",
+    "--feature", "sign-extension",
+    "-O3",
+  ], callback);
+});
+
+gulp.task("default", ["build"]);
+
+gulp.task("project:load", () => { // WebAssembly Studio only
+  const utils = require("@wasm/studio-utils");
+  utils.eval(utils.project.getFile("setup.js").getData(), {
+    logLn,
+    project,
+    monaco,
+    fileTypeForExtension,
+  });
+});
