7a8782ad15df7707e5abcc39d3dc81e58a881e8e
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-23 11:29:06 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,25 @@
+import "allocator/arena";
+
+declare function log(value: i32): void;
+
+class Base {
+	// constructor() {}   <--- comment this broke compilation --->
+	say(): void {
+		log(1);
+	}
+}
+
+class Concrete extends Base {
+	say(): void {
+		log(2);
+	}
+}
+
+function saySomething(base: Base): void {
+	base.say();
+}
+
+export function say(): void {
+	saySomething(new Base());
+	saySomething(new Concrete());
+}
