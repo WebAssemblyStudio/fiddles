22ff36b1054f9e9919a6a9d70135e3fa150d6465
WebAssemblyStudio <fiddles@webassembly.studio>
2018-05-27 18:00:17 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+// Other globally available JS functions (eg. defined on window object)
+// can be declared here, too.
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn hello(greeting: String, number: u32) -> String {
+  let answer = number * 2;
+  format!("
+    <p>
+      {} from wasm! the answer is {}!
+    </p>
+  ", greeting, answer)
+}
+
+// A struct is available as a JS class.
+#[wasm_bindgen]
+pub struct Cat {
+  name: String,
+  age: u32
+}
+
+#[wasm_bindgen]
+impl Cat {
+  pub fn new(name: String, age: u32) -> Cat {
+    Cat { name, age }
+  }
+
+  pub fn describe(&self) -> String {
+    format!("{} year(s) old cat named {}", self.age, self.name)
+  }
+}
