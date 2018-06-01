4e32f35cf87238f2e6c7fdb8c082cac6edf0f227
WebAssemblyStudio <fiddles@webassembly.studio>
2018-06-01 12:51:36 +0000

    WebAssembly Studio fiddle

---
+++ build.ts
@@ -0,0 +1,14 @@
+import * as gulp from "gulp";
+import { Service, project } from "@wasm/studio-utils";
+
+gulp.task("build", async () => {
+  const options = { cargo: true, debug: false };
+  const data = await Service.compileFiles([
+    project.getFile("src/lib.rs"),
+    project.getFile("Cargo.toml")
+  ], "rust", "wasm", options);
+  const outWasm = project.newFile("out/main.wasm", "wasm", true);
+  outWasm.setData(data["a.wasm"]);
+});
+
+gulp.task("default", ["build"], async () => {});
