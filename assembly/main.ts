bae70ffd045877bac4d56bb45af9535b4721e9df
WebAssemblyStudio <fiddles@webassembly.studio>
2018-06-02 15:28:26 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,11 @@
+export function add32(x: i32, y: i32): i32 {
+  return x + y;
+}
+
+export function add64(x: i64, y: i64): i64 {
+  return x + y;
+}
+
+export function div64(x: u64, y: u64): u64 {
+  return x / y;
+}
