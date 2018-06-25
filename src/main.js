8d51fb307f28fd5b75304c9fe6f0c617373a8a4e
WebAssemblyStudio <fiddles@webassembly.studio>
2018-06-25 22:58:43 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,14 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    ethereum_callDataSize: function() {
+      console.log("callDataSize -> 32");
+      return 32;
+    },
+    ethereum_return: function(offset, length) {
+      console.log("return(" + offset + "," + length + ")");
+    }
+  }
+}).then(result => {
+  const exports = result.instance.exports;
+  exports.main();
+}).catch(console.error);
