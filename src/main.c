76438306898438ee2cbc37252141c62a860d41ae
WebAssemblyStudio <fiddles@webassembly.studio>
2018-07-03 15:49:42 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/uio.h>
+
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main(void) {
+  printf("Hello World\n");
+}
+
+/* External function that is implemented in JavaScript. */
+extern void putcs_js(const void* c, int len);
+
+/* Basic implementation of the writev sys call. */ 
+WASM_EXPORT
+size_t writev_c(int fd, const struct iovec *iov, int iovcnt) {
+  size_t cnt = 0;
+  for (int i = 0; i < iovcnt; i++) {
+    const struct iovec *v = iov + i;
+    putcs_js(v->iov_base, v->iov_len);
+    cnt += v->iov_len;
+  }
+  return cnt;
+}
---
+++ src/main.js
@@ -0,0 +1,79 @@
+let x = '../out/main.wasm';
+
+let instance = null;
+let memoryStates = new WeakMap();
+
+function syscall(instance, n, args) {
+  switch (n) {
+    default:
+      // console.log("Syscall " + n + " NYI.");
+      break;
+    case /* brk */ 45: return 0;
+    case /* writev */ 146:
+      return instance.exports.writev_c(args[0], args[1], args[2]);
+    case /* mmap2 */ 192:
+      debugger;
+      const memory = instance.exports.memory;
+      let memoryState = memoryStates.get(instance);
+      const requested = args[1];
+      if (!memoryState) {
+        memoryState = {
+          object: memory,
+          currentPosition: memory.buffer.byteLength,
+        };
+        memoryStates.set(instance, memoryState);
+      }
+      let cur = memoryState.currentPosition;
+      if (cur + requested > memory.buffer.byteLength) {
+        const need = Math.ceil((cur + requested - memory.buffer.byteLength) / 65536);
+        memory.grow(need);
+      }
+      memoryState.currentPosition += requested;
+      return cur;
+  }
+}
+
+let s = [];
+fetch(x).then(response =>
+  response.arrayBuffer()
+).then(bytes =>
+  WebAssembly.instantiate(bytes, {
+    env: {
+      __syscall0: function __syscall0(n) { return syscall(instance, n, []); },
+      __syscall1: function __syscall1(n, a) { return syscall(instance, n, [a]); },
+      __syscall2: function __syscall2(n, a, b) { return syscall(instance, n, [a, b]); },
+      __syscall3: function __syscall3(n, a, b, c) { return syscall(instance, n, [a, b, c]); },
+      __syscall4: function __syscall4(n, a, b, c, d) { return syscall(instance, n, [a, b, c, d]); },
+      __syscall5: function __syscall5(n, a, b, c, d, e) { return syscall(instance, n, [a, b, c, d, e]); },
+      __syscall6: function __syscall6(n, a, b, c, d, e, f) { return syscall(instance, n, [a, b, c, d, e, f]); },
+      putcs_js: function (c, l) {
+        let bytes = new Uint8Array(instance.exports.memory.buffer, c, l);
+        let str = Array.from(bytes).map(e => String.fromCharCode(e)).join("");
+        let nl = str.indexOf("\n");
+        while (nl >= 0) {
+          if (s.length > 0) {
+            console.log(s.join("")+str.substring(0, nl));
+            s = [];
+          } else {
+            console.log(str.substring(0, nl));
+          }
+          str = str.substring(nl + 1);
+          nl = str.indexOf("\n");
+        }
+        if (str.length > 0) s.push(str);
+      },
+      putc_js: function (c) {
+        c = String.fromCharCode(c);
+        if (c == "\n") {
+          console.log(s.join(""));
+          s = [];
+        } else {
+          s.push(c);
+        }
+      }
+    }
+  })
+).then(results => {
+  instance = results.instance;
+  document.getElementById("container").innerText = instance.exports.main();
+}).catch(console.error);
