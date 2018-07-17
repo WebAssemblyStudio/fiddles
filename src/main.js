f61569c7c8a77b4dabba2db43440e58f2bd8f56c
WebAssemblyStudio <fiddles@webassembly.studio>
2018-07-17 18:08:33 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// Note that for webassembly.studio we're using the CLI's `--no-modules` option
+// which may not be what you're using locally (aka using ES modules/Webpack).
+// Since we're using `--no-modules` the `wasm_bindgen` global is initialized 
+// to the exports of our module, and then below initialization is slightly
+// different.
+//
+// When using wasm-bindgen locally and a different bundler like Webpack (or 
+// a different option like `--nodejs`) these integration points will be
+// tweaked slightly, and you can consult https://github.com/rustwasm/wasm-bindgen
+// for more information.
+
+const { greet } = wasm_bindgen;
+
+function runApp() {
+  greet('World');
+}
+
+// Load and instantiate the wasm file, and we specify the source of the wasm
+// file here. Once the returned promise is resolved we're ready to go and
+// use our imports.
+wasm_bindgen('../out/main_bg.wasm').then(runApp);
