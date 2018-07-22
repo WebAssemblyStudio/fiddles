39182b17f10933e280fd06e6bb54b2096249dfc8
WebAssemblyStudio <fiddles@webassembly.studio>
2018-07-22 07:48:03 +0000

    WebAssembly Studio fiddle

---
+++ assembly/integer/index.ts
@@ -0,0 +1,3 @@
+import { u128 } from "./u128";
+import { u256 } from "./u256";
+export { u128, u256 };
---
+++ assembly/main.ts
@@ -0,0 +1,7 @@
+import 'allocator/arena';
+
+import { u128, u256 } from './integer';
+export {
+  u128,
+  u256,
+};
