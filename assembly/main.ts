d7a51d4e78da82b90e822eea4572e1f21494f369
WebAssemblyStudio <fiddles@webassembly.studio>
2018-07-28 11:31:50 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,23 @@
+import "allocator/arena";
+
+class aaa {
+  time: Float64Array;
+}
+
+const bbb = new aaa();
+
+declare namespace console {
+  function log(value: i32): void;
+}
+
+export function fuck(duration: f64): void {
+  const frames = (<i32>Math.round(duration / 1000 * 60));
+  let ddd = new Float64Array(frames);
+  for (let i = 0; i < frames; i++)
+    ddd[i] = 10 / i;
+  bbb.time = ddd;
+}
+
+export function get_ptr(): aaa {
+  return bbb;
+}
