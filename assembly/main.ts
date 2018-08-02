d31f92a6dabe76240760efe4b46454204927b5db
WebAssemblyStudio <fiddles@webassembly.studio>
2018-08-02 09:03:15 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,28 @@
+import "allocator/arena";
+
+declare namespace console {
+  function logi(value: i32): void;
+  function logf(value: f64): void;
+}
+
+class A {
+  age: u64;
+}
+
+class B extends A {
+  year: u64 = 2018;
+}
+
+function assemblyArr<T extends A>(len:i32): T[] {
+  let aArr = new Array<T>(len);
+  for (let index = 0; index < len; ++index) {
+    aArr[index] = { age: index } as T;
+  }
+  return aArr;
+}
+
+let arr = assemblyArr<B>(3);
+for (let index = 0; index < arr.length; index++) {
+  console.logf(<f64>arr[index].age);
+  console.logf(<f64>arr[index].year);
+}
