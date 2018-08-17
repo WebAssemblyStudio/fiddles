0337e68ce59932ba248aa399d03015f85f75767f
WebAssemblyStudio <fiddles@webassembly.studio>
2018-08-17 07:53:09 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let argv = ['Hello,', 'world!']
+
+let argc = argv.length
+
+let exports = {
+  console: {log: console.log},
+  cakeml: {argc}
+}
+
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes, exports)).then(results => {
+  instance = results.instance;
+  document.getElementById("container").innerText = instance.exports.main();
+}).catch(console.error);
+
