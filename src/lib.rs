c149f1ff096fbbafd3eee58ce62399cfd799d526
WebAssemblyStudio <fiddles@webassembly.studio>
2018-08-17 04:58:19 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+#[wasm_bindgen]
+extern {
+    pub type Map;
+    #[wasm_bindgen(method, structural)]
+    fn get(this: &Map, key: u32) -> String;
+    fn alert(s: &str);
+}
+
+// Here we're exporting a function called `greet` which will display a greeting
+// for `name` through a dialog.
+#[wasm_bindgen]
+pub fn greet(name: &str, m: &Map) {
+    let value = m.get(1);
+    alert(&format!("Hello, {}, value: {}!", name, value));
+}
