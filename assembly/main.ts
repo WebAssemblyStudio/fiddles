2cdcfb99fa9cf85486f4e1e74e3b15746ea6b3cf
WebAssemblyStudio <fiddles@webassembly.studio>
2018-09-05 14:04:27 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,29 @@
+import "allocator/arena";
+
+declare namespace console {
+  function logi(value: i32): void;
+  function logf(value: f64): void;
+}
+
+class A {
+  getAge(): i32 {
+    return 1;
+  }
+}
+
+class B extends A {
+
+   getAge(): i32 {
+    return 2;
+  }
+}
+
+function getObject(a: A): A {
+  return a;
+}
+
+var a = getObject(new A());
+var b = getObject(new B());
+
+console.logi(a.getAge());
+console.logi(b.getAge());
