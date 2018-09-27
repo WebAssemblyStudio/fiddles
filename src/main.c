7112bc75f34d47780f7c98557b3f2d66554d2969
WebAssemblyStudio <fiddles@webassembly.studio>
2018-09-27 06:22:17 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+WASM_EXPORTsadsad
+dsadsadsa
+int main() {
+  return "dsadadsadsadsadsadasdsadasadsadsadasdsadadsasadadsad";
+}sadasdsadsa
