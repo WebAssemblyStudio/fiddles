3ac59584a15e2f19ce7b05c4ef438e29098bbae2
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-06 21:34:37 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+#[wasm_bindgen]
+extern "C" {
+
+    type SVGElement;
+    type SVGText;
+    type SVGTextPath;
+    type X;
+    
+    #[wasm_bindgen(method, js_name= viewbox)]
+    fn viewbox(this: &SVGElement, x: i32, y: i32, width: i32, height: i32 ) -> X;
+    #[wasm_bindgen(method, js_name= text)]
+    fn text(this: &SVGText, text: &str) -> SVGTextPath;
+    #[wasm_bindgen(method, js_name= path)]
+    fn path(this: &SVGTextPath, path: &str) -> SVGTextPath;
+    #[wasm_bindgen(method, js_name= animate)]
+    fn animate(this: &SVGTextPath, duration: i32, place: &str) -> SVGTextPath;
+    #[wasm_bindgen(method, js_name= plot)]
+    fn plot(this: &SVGTextPath, vector: &str) -> SVGTextPath;
+    #[wasm_bindgen(method, js_name= loop)]
+    fn _loop(this: &SVGTextPath, on: bool, ever: bool);
+    fn SVG(value: &str) -> SVGElement;
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &SVGElement);
+   
+}
+
+// Here we're exporting a function called `greet` which will display a greeting
+// for `name` through a dialog.
+#[wasm_bindgen]
+pub fn animate_code_dive() {
+    let svg = SVG("drawing");
+    log(&svg);
+}
