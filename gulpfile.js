b79cfaee6fb6c5b43875b9314892bdfdd8823c0e
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-07 02:33:41 +0000

    WebAssembly Studio fiddle

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require("gulp");
+
+gulp.task("build", callback => {
+  const asc = require("assemblyscript/bin/asc");
+  asc.main([
+    "main.ts",
+    "--baseDir", "assembly",
+    "--binaryFile", "../out/main.wasm",
+    "--importMemory",
+    "--sourceMap",
+    "--optimize",
+    "--measure"
+  ], callback);
+});
+
+gulp.task("default", ["build"]);
+
+gulp.task("project:load", () => { // WebAssembly Studio only
+  const utils = require("@wasm/studio-utils");
+  eval(utils.project.getFile("setup.js").getData());
+});
