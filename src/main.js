aca8443c2da2a880a8f075d6307bb6ba664cca99
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-07 00:36:14 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const { animate_code_dive } = wasm_bindgen;
+
+function runApp() {
+   animate_code_dive("My first ----- Code::Dive ---- demo");
+}
+
+// Load and instantiate the wasm file, and we specify the source of the wasm
+// file here. Once the returned promise is resolved we're ready to go and
+// use our imports.
+wasm_bindgen('../out/main_bg.wasm').then(runApp).catch(console.error);
