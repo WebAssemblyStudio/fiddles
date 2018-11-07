a09d6af1ef1d2ab33d78b6a9018e7d57ab940857
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-07 13:22:28 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,12 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+
+export function add1(x: i32, y: i32): i32 {
+  return x + y;
+}
+
