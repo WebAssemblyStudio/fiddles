c38229cfbd48feadf69fc7347e6f8861b6a84b4f
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-08 09:12:53 +0000

    WebAssembly Studio fiddle

---
+++ src/chart.js
@@ -0,0 +1,62 @@
+
+
+let wasmTime = 0, jsTime = 0;
+const chartOptions = {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Perf"
+	},
+	axisY: {
+    minimum: 0.00001,
+    maximum: 0.1,
+		title: "ms"
+	},
+	data: [{        
+		type: "column",  
+		showInLegend: true, 
+		legendMarkerColor: "grey",
+		dataPoints: [      
+			{ y: 0, label: "Wasm" },
+			{ y: 0,  label: "Js" },
+		]
+	}]
+};
+
+const chart = new CanvasJS.Chart("chartContainer", chartOptions);
+
+const runWasmFnNTimes = (n) => (func)=> {
+  return new promise((resolve)=>{
+let total=0;
+for(let i =0; i< n; i++){
+time = performance.now();
+const result = func();
+time = performance.now() - time;
+total = total + time;
+}
+chartOptions.data[0].dataPoints[0].y = total/10;
+chart.render();
+resolve(result);
+  });
+};
+
+const runWasmFnOneTime = runWasmFnNTimes(1);
+const runWasmFn100Times = runWasmFnNTimes(100);
+
+const runJsFnNTimes = (n)=> (func)=> {
+ return new promise((resolve)=>{
+    let total=0;
+for(let i =0; i< n; i++){
+jsTime = performance.now();
+const result = func();
+jsTime = performance.now() - jsTime;
+total = total + jsTime;
+}
+chartOptions.data[0].dataPoints[1].y = total/10;
+chart.render();
+resolve(result);
+  });
+
+};
+const runJsFnOneTime = runJsFnNTimes(1);
+const runJsFn100Times = runJsFnNTimes(100);
