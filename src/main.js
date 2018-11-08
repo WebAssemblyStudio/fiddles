a4c50d1f05ffcac73d4cdd25357a033df9dc0e3b
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-08 09:54:06 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const { fibonacci_reccursive } = wasm_bindgen;
+const run = ()=> {
+  
+ runWasmFnOneTime(()=> fibonacci_reccursive(40));
+  
+ runJsFnOneTime(()=>{ 
+   fibonacci(40);
+  });
+
+};
+
+const fibonacci = (num) => {
+  if (num <= 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
+wasm_bindgen('../out/main.wasm').then(() => run());
+
