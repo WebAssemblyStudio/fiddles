3d4e816b5f3b169a0ee6ddd3632962019e9af38d
WebAssemblyStudio <fiddles@webassembly.studio>
2018-11-19 05:56:51 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,18 @@
+declare function sayHello(): void;
+
+var left = "key1";
+var right = "key2";
+
+
+assert(left == right);
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+
+export function compare(x: string, y: string): bool {
+  return (x == y);
+}
+
