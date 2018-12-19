2b32efc6a5d0363d7cea834111202fe766866c95
WebAssemblyStudio <fiddles@webassembly.studio>
2018-12-19 07:54:13 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,12 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  document.getElementById("container").textContent = instance.exports.getRandomNumber(0, 10);
+  document.getElementById("container1").textContent = instance.exports.getXFromAngle(1, 1, 1, 1);
+  document.getElementById("container2").textContent = instance.exports.getYFromAngle(1, 1, 1, 1);
+  document.getElementById("container3").textContent = instance.exports.getAngle(1, 2, 2, 3, true);
+  document.getElementById("container4").textContent = instance.exports.getAngle(1, 2, 2, 3, false);
+  document.getElementById("container5").textContent = instance.exports.getPercentageFromNumber(33, 20, 50);
+  document.getElementById("container6").textContent = instance.exports.getNumberFromPercentage(33, 20, 50);
+}).catch(console.error);
