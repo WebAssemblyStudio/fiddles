a09cd703f7b9ed7e67f076738ca6df8067440cc9
WebAssemblyStudio <fiddles@webassembly.studio>
2018-12-31 15:06:09 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,25 @@
+declare function _logi(x: i32): void;
+declare function _logf(x: f64): void;
+declare function _log_ref(x: usize): void;
+declare function _log_str(x: string): void;
+
+export function log<T>(item: T): void {
+  if (isReference<T>()) {
+    if (isString<T>()) {
+      _log_str(changetype<string>(item));
+    } else {
+      _log_ref(changetype<usize>(item));
+    }
+  } else {
+    if (isInteger<T>()) {
+      _logi(<i32>item);
+    } else {
+      _logf(<f64>item);
+    }
+  }
+}
+
+log("abc");
+log(123);
+log(123.0);
+
