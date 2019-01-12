ac0f9617a692604c9c2f70be9ed73a1a1e7df28e
WebAssemblyStudio <fiddles@webassembly.studio>
2019-01-12 11:34:16 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,18 @@
+import "allocator/arena";
+
+declare function log(v: u32): void;
+
+class Base {
+  constructor(public id: u32 = 0) {}
+}
+
+export class Foo extends Base {
+  readonly SOME: u32 = 1;
+  constructor() {
+    super();
+  }
+}
+
+var foo = new Foo();
+log(foo.SOME);
+
