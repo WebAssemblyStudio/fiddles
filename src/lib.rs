b652a256ad9851d4ff4a0b51ed1c4fe00acdcd16
WebAssemblyStudio <fiddles@webassembly.studio>
2019-01-16 16:01:49 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// lifted from the `console_log` example
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct Counter {
+    key: char,
+    count: i32,
+}
+
+#[wasm_bindgen]
+impl Counter {
+    pub fn default() -> Counter {
+        log("Counter::default");
+        Self::new('a', 0)
+    }
+    pub fn new(key: char, count: i32) -> Counter {
+        log(&format!("Counter::new({}, {})", key, count));
+        Counter {
+            key: key,
+            count: count,
+        }
+    }
+
+    pub fn key(&self) -> char {
+        log("Counter.key()");
+        self.key
+    }
+
+    pub fn count(&self) -> i32 {
+        log("Counter.count");
+        self.count
+    }
+
+    pub fn increment(&mut self) {
+        log("Counter.increment");
+        self.count += 1;
+    }
+
+    pub fn update_key(&mut self, key: char) {
+        self.key = key;
+    }
+}
