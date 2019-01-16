d144a74439e08fc2d1666cf5d50339bf9127e252
WebAssemblyStudio <fiddles@webassembly.studio>
2019-01-16 04:08:31 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,4 @@
+export function conv(x: f64): u32 {
+  return <u32>x;
+}
+
---
+++ src/main.js
@@ -0,0 +1,12 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(msg, file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  let res = exports.conv(1e11);
+  console.log(res >>> 0);
+
+}).catch(console.error);
