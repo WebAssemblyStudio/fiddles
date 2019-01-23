90be675b89c6a26efb18fd054682ce7427bd0d22
WebAssemblyStudio <fiddles@webassembly.studio>
2019-01-23 00:24:19 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,18 @@
+ 
+export function imulOriginal(a: f64, b: f64): f64 {
+  return Math.imul(a, b);
+}
+
+export function imulFixed(a: f64, b: f64): f64 {
+  var x = <i32><i64>(a - 4294967296 * Math.floor(a * (1.0 / 4294967296)));
+  var y = <i32><i64>(b - 4294967296 * Math.floor(b * (1.0 / 4294967296)));
+  return x * y;
+}
+
+export function bench(): f64 {
+  var acc = 0.0
+  for (let i = 0; i < 1000000; i++) {
+    acc += imulFixed(2.0, <f64>i);
+  }
+  return acc;
+}
