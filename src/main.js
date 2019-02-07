95353c0b01e52469deb3fb20074d70089023c0ad
WebAssemblyStudio <fiddles@webassembly.studio>
2019-02-07 22:01:51 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,14 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(msg, file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+  console: {
+    logi(value) { console.log('logi: ' + value); },
+    logf(value) { console.log('logf: ' + value); }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  // console.log(exports.getAge());
+}).catch(console.error);
