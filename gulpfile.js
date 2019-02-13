3d6e4bf127b3a82fcac5dc85d7c56ec1828eaaa6
WebAssemblyStudio <fiddles@webassembly.studio>
2019-02-13 19:21:00 +0000

    WebAssembly Studio fiddle

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require("gulp");
+
+gulp.task("build", callback => {
+  const asc = require("assemblyscript/bin/asc");
+  const aspect = require("as-pect/lib/cli");
+  // console.log(aspect);
+  aspect(["--config", "./as-pect.config.js"]);
+  asc.main([
+    "main.ts",
+    "--baseDir", "assembly",
+    "--binaryFile", "../out/main.wasm",
+    "--sourceMap",
+    "--measure"
+  ], callback);
+});
+
+
+
+gulp.task("default", ["build"]);
+
+// This task is not required when running the project locally. Its purpose is to set up the
+// AssemblyScript compiler when a new project has been loaded in WebAssembly Studio.
+gulp.task("project:load", () => {
+  const utils = require("@wasm/studio-utils");
+  utils.eval(utils.project.getFile("setup.js").getData(), {
+    logLn,
+    project,
+    monaco,
+    fileTypeForExtension,
+  });
+});
