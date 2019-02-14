01793a4094817a28989185caf7a8f2394c1ae9fc
WebAssemblyStudio <fiddles@webassembly.studio>
2019-02-14 13:17:28 +0000

    WebAssembly Studio fiddle

---
+++ assembly/A.ts
@@ -0,0 +1,8 @@
+import {ClassB} from "./B";
+export {ClassB};
+
+export class ClassA {
+    doA():f32 {
+        return 123.0;
+    }
+}
---
+++ assembly/B.ts
@@ -0,0 +1,5 @@
+export class ClassB {
+    dob():f32 {
+        return 123456.0;
+    }
+}
