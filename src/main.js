f6da3c70e71556da112f80f2e664bb29d2393ed5
WebAssemblyStudio <fiddles@webassembly.studio>
2019-02-27 10:44:16 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,23 @@
+var importObject = {
+		env: {
+			memory: new WebAssembly.Memory({ initial: 10 })
+		}
+};
+
+var wasmMemoryF64Array = new Float64Array(importObject.env.memory.buffer);
+
+let array = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+array.forEach((element, idx) =>
+		wasmMemoryF64Array[idx] = element);
+
+fetch('../out/binarySearch.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes, importObject)).then(results => {
+  let instance = results.instance;
+  let binarySearch = instance.exports.binarySearch;
+  let result = binarySearch(array.length - 1, 70);
+
+  document.getElementById("container").textContent = result;
+}).catch(console.error);
+
