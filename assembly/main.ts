431ed742dbc51cd4b2ebf20970dba619f59c3163
WebAssemblyStudio <fiddles@webassembly.studio>
2019-03-04 09:13:55 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,11 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function createPixel(w: u16, h: u16): void {
+    let length = w * h;
+}
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
