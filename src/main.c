7b1915d5f8cf45f8a64322a10e612dc180184071
WebAssemblyStudio <fiddles@webassembly.studio>
2019-03-09 14:06:22 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main() {
+  return 42;
+}
+
+WASM_EXPORT
+int add(int a, int b){
+  return a + b;
+}
+
+WASM_EXPORT
+int div(int a , int b ) {
+  return a / b;
+}
