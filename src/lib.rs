5b2a751d935ffad08844e55ed17f9468b7a81df0
WebAssemblyStudio <fiddles@webassembly.studio>
2019-04-17 08:03:07 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+fn find_fibonacci(n: usize) -> i64 {
+  let mut f:Vec<i64> = vec![0, 1];
+  for i in 2..n + 1 {
+    let next_f = f[i - 1] + f[i - 2];
+    f.push(next_f)
+  }
+  return f[n];
+}
+
+#[wasm_bindgen]
+pub fn fibonacci(n: usize) -> i64 {
+  let ans: i64 = find_fibonacci(n);
+  println!("{}", ans);
+  ans
+}
---
+++ src/main.js
@@ -0,0 +1,41 @@
+const { fibonacci } = wasm_bindgen
+
+function runApp() {
+  const target = 107
+  console.log('Starting run...................................')
+
+  //wasm with memoization
+  let t = performance.now()
+  console.log(fibonacci(target))
+  console.log(`wasm: ${performance.now() - t}`)
+
+  //es6 with tail recursion
+  t = performance.now()
+  console.log(fibTailRecursion(target))
+  console.log(`es6 tail: ${performance.now() - t}`)
+
+  //es6 with memoization
+  t = performance.now()
+  console.log(fibMemoized(target))
+  console.log(`es6 memo: ${performance.now() - t}`)
+}
+
+function fibTailRecursion(n, a=0, b=1) {
+  if (n === 0) {
+    return a
+  }
+  if (n === 1) {
+    return b
+  }
+  return fibTailRecursion(n-1, b, a+b)
+}
+
+function fibMemoized(n) {
+  const f = [0, 1];
+  for (let i=2; i<=n; i++) {
+    f.push(f[i-1] + f[i-2])
+  }
+  return f.pop()
+}
+
+wasm_bindgen('../out/main_bg.wasm').then(runApp).catch(console.error)
