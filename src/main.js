e662de6ea3ee0469ec39e821ed9e50dd4758fc89
WebAssemblyStudio <fiddles@webassembly.studio>
2019-05-08 13:26:37 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function squareRoot() {
+  fetch('../out/main.wasm').then(response =>
+    response.arrayBuffer()
+  ).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+    instance = results.instance;
+    document.getElementById("result").textContent = "Result: " + instance.exports.squareRoot(document.getElementById("radicand").value);
+  }).catch(console.error);
+}
