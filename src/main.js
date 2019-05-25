cdda65fad337aa8db8dbd621aee8f4413df32d9b
WebAssemblyStudio <fiddles@webassembly.studio>
2019-05-25 12:24:06 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,9 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  // document.getElementById("container").textContent = instance.exports.add(1,1);
+  document.getElementById("container1").textContent = instance.exports.addf(.1,.2);
+  document.getElementById("container2").textContent = instance.exports.addcf();
+}).catch(console.error);
+
---
+++ src/main.wat
@@ -0,0 +1,19 @@
+(module
+  (func $add (param $lhs i32) (param $rhs i32) (result i32)
+    get_local $lhs
+    get_local $rhs
+    i32.add)
+  (export "add" (func $add))
+
+  (func $addf (param $lhs f32) (param $rhs f32) (result f32)
+    get_local $lhs
+    get_local $rhs
+    f32.add)
+  (export "addf" (func $addf))
+
+  (func $addcf (result f32)
+    f32.const 0.1
+    f32.const 0.2
+    f32.add)
+  (export "addcf" (func $addcf))
+)
