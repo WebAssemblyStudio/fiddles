59b5e7661bda7232de3bea146cf70d092ed8cab5
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-17 18:30:29 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,11 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  const ptr = exports.createMatrix(32, 32)
+  console.log('mathix pointer' + ptr);
+}).catch(console.error);
