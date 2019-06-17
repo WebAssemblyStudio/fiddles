b55fc76519dfa32c327d3831bfc3a60d59de6f9b
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-17 18:43:08 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,6 @@
+
+export function createMatrix(rows: i32, cols: i32): f64[][] {
+  return new Array<f64[]>(rows);
+}
+
+
---
+++ src/main.js
@@ -0,0 +1,11 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  const ptr = exports.createMatrix(2, 2);
+  console.log('mathix pointer: ' + ptr);
+}).catch(console.error);
