b0c0b053d5a0eabf8ade15208e5444ad18a36083
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-17 19:04:26 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,11 @@
+
+export function createMatrix(rows: i32, cols: i32): f64[][] {
+   const data: f64[][] = [];
+   for (let i = 0; i < rows; i++) {
+      data.push(new Array<f64>(cols));
+      for (let j = 0; j < cols; j++) {
+        data[i][j] = Math.random() * 2 - 1;
+      }
+   }     
+   return data;
+}
