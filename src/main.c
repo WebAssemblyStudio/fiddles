f895df3fb8f148e5115da4918a7130f048ad0555
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-20 06:52:06 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main() {
+  return 66;
+}
+
+
+WASM_EXPORT
+const char* getJson(){
+  return "Türkçe fıstıkçı şahap";
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+fetch('main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  document.getElementById("container").textContent = instance.exports.getJson();
+}).catch(console.error);
