04c77b89ffe19ddb8acfdcf905d768e2614df226
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-20 15:16:54 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,51 @@
+//fetch('../out/main.wasm').then(response => response.arrayBuffer()).then(bytes => WebAssembly.instantiate(bytes)).then(r => gisExpress.doWork(r)).catch(console.error);
+
+var gisExpress = {
+   doWork : function(r)
+   {
+    wasm = r.instance.exports;
+    document.getElementById("container").textContent = gisExpress.asString(gisExpress.asArray(new Uint8Array(wasm.memory.buffer), wasm.getJson()));
+  },
+
+  fileChanged : function ()
+  {
+    var reader = new FileReader();
+    reader.onload = function () { gisExpress.fileLoad(this.result); };
+    reader.readAsArrayBuffer(this.files[0]);
+  },
+
+  fileLoad : function (r)
+  {
+    var array = new Uint8Array(r);
+    
+    WebAssembly.instantiateStreaming(fetch('../out/main.wasm')).then(r => {
+      var wasm = r.instance.exports;
+      var i8 = new Uint8Array(wasm.memory.buffer);
+
+alert(wasm.allocate(array.length));
+
+      var i32 = new Uint32Array(wasm.allocate(array.length));
+      
+      for (var i = 0; i < array.length; i++) {
+        i32[i] = array[i];
+      }
+
+      alert(wasm.fileLoad());
+      //alert(gisExpress.asString(gisExpress.asArray(i8, wasm.fileLoad())));
+    });
+  },
+
+  asArray : function(h, p) 
+  {
+    let arr = []; let n = 0;
+    for (i = p; h[i]; i++) { arr[n++] = h[i]; }
+    return arr;
+  },
+
+  asString : function(uintArray) 
+  {
+      return decodeURIComponent(escape(String.fromCharCode.apply(null, uintArray)));
+  }
+}
+
+document.getElementById('fileOpen').addEventListener('change', gisExpress.fileChanged, false);
