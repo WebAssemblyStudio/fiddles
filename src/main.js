d46e9b8fe63179809fb9141e89f57b85e3025c6a
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-20 08:09:10 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,37 @@
+fetch('../out/main.wasm').then(response => response.arrayBuffer()).then(bytes => WebAssembly.instantiate(bytes)).then(r => gisExpress.doWork(r)).catch(console.error);
+
+let wasm;
+
+var gisExpress = {
+   doWork : function(r)
+   {
+    wasm = r.instance.exports;
+    document.getElementById("container").textContent = gisExpress.asString(gisExpress.asArray(new Uint8Array(r.instance.exports.memory.buffer), wasm.test()));
+  },
+
+  fileChanged : function ()
+  {
+    var reader = new FileReader();
+    reader.onload = function () { gisExpress.fileLoad(new Uint8Array(this.result)); };
+    reader.readAsArrayBuffer(this.files[0]);
+  },
+
+  fileLoad : function (r)
+  {
+    alert(r);
+  },
+
+  asArray : function(h, p) 
+  {
+    let arr = []; let n = 0;
+    for (i = p; h[i]; i++) { arr[n++] = h[i]; }
+    return arr;
+  },
+
+  asString : function(uintArray) 
+  {
+      return decodeURIComponent(escape(String.fromCharCode.apply(null, uintArray)));
+  }
+}
+
+document.getElementById('fileOpen').addEventListener('change', gisExpress.fileChanged, false);
