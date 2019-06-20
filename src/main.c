01100172ae10f1c612f770245330a1ad945254a8
WebAssemblyStudio <fiddles@webassembly.studio>
2019-06-20 10:26:09 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main() {
+  return 42;
+}
+
+WASM_EXPORT
+char * test() {
+  return "Türkçe Fıstıkçı Şahap";
+}
+
+WASM_EXPORT
+int fileLoad(char *buffer, int size)
+{
+  for(int i = 0; i < size ; i++)
+  {
+    if((*buffer)[i] > 0) return (*buffer)[i];
+  }
+
+  return 0;
+}
---
+++ src/main.js
@@ -0,0 +1,47 @@
+fetch('../out/main.wasm').then(response => response.arrayBuffer()).then(bytes => WebAssembly.instantiate(bytes)).then(r => gisExpress.doWork(r)).catch(console.error);
+
+let wasm;
+
+var gisExpress = {
+   doWork : function(r)
+   {
+    wasm = r.instance.exports;
+    document.getElementById("container").textContent = gisExpress.asString(gisExpress.asArray(new Uint8Array(r.instance.exports.memory.buffer), wasm.test()));
+  },
+
+  fileChanged : function ()
+  {
+    var reader = new FileReader();
+    reader.onload = function () { gisExpress.fileLoad(this.result); };
+    reader.readAsArrayBuffer(this.files[0]);
+  },
+
+  fileLoad : function (r)
+  {
+    var array = new Uint8Array(r);
+
+    for(var i = 0; i < array.length ; i++)
+      {
+        if(array[i] > 0) {
+          alert(array[i])
+          break;
+        };
+      }
+
+        alert(wasm.fileLoad(array, array.length));
+      },
+
+  asArray : function(h, p) 
+  {
+    let arr = []; let n = 0;
+    for (i = p; h[i]; i++) { arr[n++] = h[i]; }
+    return arr;
+  },
+
+  asString : function(uintArray) 
+  {
+      return decodeURIComponent(escape(String.fromCharCode.apply(null, uintArray)));
+  }
+}
+
+document.getElementById('fileOpen').addEventListener('change', gisExpress.fileChanged, false);
