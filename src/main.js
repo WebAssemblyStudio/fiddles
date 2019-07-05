8dbd59747e22afaa245f218eb8c9be8aab1579a1
WebAssemblyStudio <fiddles@webassembly.studio>
2019-07-05 05:58:37 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,11 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  // console.log('ptr: ' + exports.createUint8Array(6553600)); // OK
+  console.log('ptr: ' + exports.createUint8Array(6553600 + 1)); // RuntimeError
+}).catch(console.error);
