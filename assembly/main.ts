8b0b0dd1a650e2516779e76d608a1857b53ccf86
WebAssemblyStudio <fiddles@webassembly.studio>
2019-07-29 22:44:58 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,75 @@
+declare function sayHello(): void;
+
+sayHello();
+
+declare function setBignumStack(startData: i32, len: i32): void;
+
+// 0x6001600101600055
+// [ 96, 1, 96, 1, 1, 96, 0, 85 ]
+
+// bignum stack size is 100 elements
+// each stack element is 32 bytes
+let BignumStackSize = 100;
+let BignumElementSize = 32;
+let BignumStack = new ArrayBuffer(BignumElementSize*BignumStackSize);
+//let BignumStackPtr = changetype<usize>(BignumStack);
+//setBignumStack(BignumStackPtr, 100);
+
+//let BignumStackElements = new Array<Uint8Array>(100);
+let BignumStackElements = Array.create<Uint8Array>(100);
+
+for (let i = 0; i < BignumStackSize; i++) {
+  BignumStackElements[i] = Uint8Array.wrap(BignumStack, i*BignumElementSize, 32);
+}
+
+let BignumStackTop = 0;
+
+
+
+let code_array: u8[] = [96, 1, 96, 1, 1, 96, 0, 85];
+let result: i32 = 0;
+let pc: i32 = 0;
+
+const push1: u8 = 0x60;
+const add: u8 = 0x01;
+const sstore: u8 = 0x85;
+
+export function run_evm(): i32 {
+  //code_array = [ 96, 1, 96, 1, 1, 96, 0, 85 ]
+
+  while (pc < code_array.length) {
+    let opcode: i8 = code_array[pc];
+    pc++;
+    switch (opcode) {
+      case push1:
+        result = result + 1;
+        let push_val = code_array[pc];
+        // now that the value has been read, advance pc to the next opcode
+        pc = pc + 1;
+
+        let stack_slot = BignumStackElements[BignumStackTop];
+        // 1 byte value goes in the last byte of the 32-byte stack slot
+        stack_slot[31] = push_val;
+
+        BignumStackTop++;
+        break;
+
+      case add:
+        BignumStackElements[BignumStackTop-1]
+        BignumStackElements[BignumStackTop-2];
+        add256();
+        
+        break;
+
+      case sstore:
+        break;
+
+      default:
+        break;
+    }
+
+    //pc++;
+  }
+
+  return result;
+}
