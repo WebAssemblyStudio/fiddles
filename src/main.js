f5974d145e7cce88a54e1fad24c6c072ca070628
WebAssemblyStudio <fiddles@webassembly.studio>
2019-08-05 12:05:15 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,17 @@
+console.log('window.loader:', loader)
+
+loader.instantiateStreaming(fetch("../out/main.wasm"), {
+  main: {
+    sayHello() {
+      console.log("Hello from WebAssembly!");
+    }
+  },
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  document.getElementById("container").textContent = "Result: " + exports.greet(3);
+}).catch(console.error);
