3c8baf766e7b2909a844232af03113b30b2051d8
WebAssemblyStudio <fiddles@webassembly.studio>
2019-08-09 13:59:41 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,24 @@
+async function main() {
+
+  let { instantiateStreaming } = await import('https://cdn.jsdelivr.net/gh/cdetrio/assemblyscript@es6-export/lib/loader/index.js');
+
+  //WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  instantiateStreaming(fetch("../out/main.wasm"), {
+    main: {
+      sayHello() {
+        console.log("Hello from WebAssembly!");
+      }
+    },
+    env: {
+      abort(_msg, _file, line, column) {
+        console.error("abort called at main.ts:" + line + ":" + column);
+      }
+    },
+  }).then(exports => {
+    memory = exports.memory;
+    document.getElementById("container").textContent = "Result: " + exports.__getString(exports.greet(3));
+  }).catch(console.error);
+
+}
+
+main();
