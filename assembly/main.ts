941cff5f70e74437f03a7dfb3758120a50048931
WebAssemblyStudio <fiddles@webassembly.studio>
2019-08-22 22:48:36 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,33 @@
+export function isqrt32_1(x: u32): u32 {
+  if (x < 2) return x;
+  var s = 2;
+  var xs = x >> 2;
+  while (xs && xs != x) {
+    s += 2;
+    xs = x >> s;
+  }
+  s -= 2;
+  var ux = x;
+  var res: u32 = 0;
+  while (s >= 0) {
+    res <<= 1;
+    let m = res + 1;
+    if (m * m <= ux >> s) {
+      res = m;
+    }
+    s -= 2;
+  }
+  return res;
+}
+
+export function isqrt32_2(value: u32): u32 {
+  if (!value) return 0;
+  let xn: u32 = 1 << ((32 - clz(value)) >> 1);
+  xn = (xn + value / xn) >> 1;
+  xn = (xn + value / xn) >> 1;
+  xn = (xn + value / xn) >> 1;
+  xn = xn * xn > value ? xn - 1 : xn;
+  return xn;
+}
+
+
