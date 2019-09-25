98bbac157170ca1f2b13501c8843aa731da04764
WebAssemblyStudio <fiddles@webassembly.studio>
2019-09-25 23:39:38 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,33 @@
+export function memcmp(vl: usize, vr: usize, n: usize): i32 {
+  if (vl == vr) return 0;
+  while (n != 0 && load<u8>(vl) == load<u8>(vr)) {
+    n--; vl++; vr++;
+  }
+  return n ? <i32>load<u8>(vl) - <i32>load<u8>(vr) : 0;
+}
+
+export function memcmp2(vl: usize, vr: usize, n: usize): i32 {
+  if (vl == vr) return 0;
+  if ((vl & 7) == (vr & 7)) {
+    while (vl & 7) {
+      if (!n) return 0;
+      let a = <i32>load<u8>(vl);
+      let b = <i32>load<u8>(vr);
+      if (a != b) return a - b;
+      vl++; vr++; n--;
+    }
+    while (n >= 8) {
+      if (load<u64>(vl) != load<u64>(vr)) break;
+      vl += 8;
+      vr += 8;
+      n  -= 8;
+    }
+  }
+  while (n--) {
+    let a = <i32>load<u8>(vl);
+    let b = <i32>load<u8>(vr);
+    if (a != b) return a - b;
+    vl++; vr++;
+  }
+  return 0;
+}
