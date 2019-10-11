06464c84ec5e1f4a63910398989295fd9eb44ca7
WebAssemblyStudio <fiddles@webassembly.studio>
2019-10-11 16:36:01 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,13 @@
+
+
+export function div_s(x: i32): i32 {
+  return x / 4;
+}
+
+export function div_u(x: u32): u32 {
+  return x / 4;
+}
+
+export function div_u2(x: u32): u32 {
+  return x >> 2;
+}
