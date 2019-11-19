781c07c7bb13ead98c367d3839ec2a4bcaa2be18
WebAssemblyStudio <fiddles@webassembly.studio>
2019-11-19 05:04:48 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <sys/uio.h>
+
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main(void) {
+
+    int r[2800 + 1];
+    int i, k;
+    int b, d;
+    int c = 0;
+
+    for (i = 0; i < 28000; i++) {
+        r[i] = 2000;
+    }
+
+    for (k = 2800; k > 0; k -= 14) {
+        d = 0;
+
+        i = k;
+        for (;;) {
+            d += r[i] * 10000;
+            b = 2 * i - 1;
+
+            r[i] = d % b;
+            d /= b;
+            i--;
+            if (i == 0) break;
+            d *= i;
+        }
+        printf("%.4d", c + d / 10000);
+        c = d % 10000;
+    }
+
+    //return 0;
+
+  
+  printf("Hello World\n");
+}
+
+/* External function that is implemented in JavaScript. */
+extern void putc_js(char c);
+
+/* Basic implementation of the writev sys call. */ 
+WASM_EXPORT
+size_t writev_c(int fd, const struct iovec *iov, int iovcnt) {
+  size_t cnt = 0;
+  for (int i = 0; i < iovcnt; i++) {
+    for (int j = 0; j < iov[i].iov_len; j++) {
+      putc_js(((char *)iov[i].iov_base)[j]);
+    }
+    cnt += iov[i].iov_len;
+  }
+  return cnt;
+}
