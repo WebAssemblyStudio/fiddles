e93f34e03442ea35e2dee239b61fa87eeb84dafd
WebAssemblyStudio <fiddles@webassembly.studio>
2019-12-05 10:08:03 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,39 @@
+// import { exp2f_lut } from "./exp2f";
+
+// export function exp2_lut(x: f32): f32 {
+//   return exp2f_lut(x);
+// }
+
+// export function expf(x: f32): f32 {
+//   return Mathf.exp(x);
+// }
+
+// import { logf_lut } from "./logf";
+
+// export function log_lut(x: f32): f32 {
+//   return logf_lut(x);
+// }
+
+// export function logf(x: f32): f32 {
+//   return Mathf.log(x);
+// }
+
+// import { expf_lut } from "./expf";
+
+// export function exp_lut(x: f32): f32 {
+//   return expf_lut(x);
+// }
+
+// export function expf(x: f32): f32 {
+//   return Mathf.exp(x);
+// }
+
+import { powf_lut } from "./powf";
+
+export function pow_lut(x: f32, y: f32): f32 {
+  return powf_lut(x, y);
+}
+
+// export function powf(x: f32, y: f32): f32 {
+//   return Mathf.pow(x, y);
+// }
