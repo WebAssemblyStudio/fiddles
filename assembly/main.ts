3f2602e6376ef2bc32cc7a7fe8bed710a4891d24
WebAssemblyStudio <fiddles@webassembly.studio>
2019-12-08 01:35:34 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,11 @@
+export function testShortcutAnd(a: i64, b: i32): bool {
+  return bool((a >> 63) && b);
+}
+
+export function testShortcutAnd2(a: i64, b: i32): bool {
+  return bool(i32(a >> 63) && b);
+}
+
+export function testShortcutAnd3(a: i64, b: i32): bool {
+  return bool((a >> 63) & b);
+}
