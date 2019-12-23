5bed88f03b083dc3ae11e76c1225f7d28414ca99
WebAssemblyStudio <fiddles@webassembly.studio>
2019-12-23 16:13:32 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,5 @@
+const initializedArray: i32[] = new Array();
+
+export function willFail(): void {
+  initializedArray.push(1);
+} 
---
+++ src/main.js
@@ -0,0 +1,16 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  main: {
+    sayHello() {
+      console.log("Hello from WebAssembly!");
+    }
+  },
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  exports.__start();
+  exports.willFail();
+}).catch(console.error);
