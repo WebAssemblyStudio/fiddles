bf24660953e1994aa506c427efa9be1a24961763
WebAssemblyStudio <fiddles@webassembly.studio>
2019-12-28 16:22:34 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,21 @@
+class A1 {
+    private i32_0!: i32;
+    private i32_1!: i32;
+    private c1_0!: C1;
+    private i32_2!: i32;
+    private c1_1!: C1;
+}
+
+class A2 extends A1 { }
+
+class B1 {
+    private a1!: A1;
+}
+
+class C1 {
+    private a2!: A2;
+}
+
+const poolB: B1[] = [];
+const poolA: A2[] = [];
+poolA.push(new A2());
