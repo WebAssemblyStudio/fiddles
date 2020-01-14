93b232b748f3f0992e709eb4bd93e228a43ca306
WebAssemblyStudio <fiddles@webassembly.studio>
2020-01-14 06:55:19 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main()
+{
+ 
+  float c;
+  int a = 345;
+  int b = 6843;
+ 
+  c = sqrt(a*a + b*b);
+
+  return c;
+}
