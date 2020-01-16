672980d8cbce4a164c32b7152639742f75401e08
WebAssemblyStudio <fiddles@webassembly.studio>
2020-01-16 23:54:51 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,46 @@
+{
+  // let arrays: i32[][] = [[], []];
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+  // arrays[0].push(0);
+
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[0].push(0);
+  // arrays[1].push(0);
+  // arrays[1].push(0);
+  // arrays[0].push(0);
+
+  let arrays0: i32[] = [];
+  let arrays1: i32[] = [];
+  arrays0.push(0);
+  arrays0.push(0);
+  arrays0.push(0);
+  arrays0.push(0);
+  arrays0.push(0);
+  arrays0.push(0);
+  arrays0.push(0);
+  arrays0.push(0);
+
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays0.push(0);
+  arrays1.push(0);
+  arrays1.push(0);
+  arrays0.push(0);
+}
