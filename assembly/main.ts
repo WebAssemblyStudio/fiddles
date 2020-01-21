14a64227b1a19959dc99d6f35932f21ca4003bdd
WebAssemblyStudio <fiddles@webassembly.studio>
2020-01-21 23:10:39 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,18 @@
+export function say(hello: string): string {
+  return hello + " world"
+}
+
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
+
+export function teste(): string {
+  let x = "ZZZZZZZZZZZZ"
+  return x;
+}
+
+export class X {
+  tester2(): String {
+    return "Aew";
+  }
+}
