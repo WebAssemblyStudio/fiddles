6584090a25b88e4afc026f803afaf7e073b5a79d
WebAssemblyStudio <fiddles@webassembly.studio>
2020-01-23 12:34:27 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,16 @@
+(async () => {
+  const codePromise = fetch('../out/main.wasm');
+  const { instance } = await WebAssembly.instantiateStreaming(codePromise);
+
+  const buffer = new Uint8Array(instance.exports.memory.buffer);
+
+  const pointer  = instance.exports.helloWorld();
+
+  let string = ""
+
+  for(let i = pointer; buffer[i]; i++) {
+    string += String.fromCharCode(buffer[i])
+  }
+
+  document.getElementById("container").textContent = string;
+})()
