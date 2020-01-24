f6e4858b140a797c193e7b9b9fb2edfc1b397ce5
WebAssemblyStudio <fiddles@webassembly.studio>
2020-01-24 16:15:29 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,15 @@
+export function fromHexString(hex: string): Uint8Array {
+  assert(hex.length % 2 == 0, 'input ' + hex + ' has odd length')
+  // Skip possible `0x` prefix.
+  if (hex.length >= 2 && hex[0] == '0' && hex[1] == 'x') {
+    hex = hex.substr(2)
+  }
+  let output = new Uint8Array(hex.length / 2);
+  for (let i = 0; i < hex.length; i += 2) {
+    output[i / 2] = I8.parseInt(hex.substr(i, 2), 16)
+  }
+  return output;
+}
+
+let zero = fromHexString("0x00");
+assert(zero[0] == 0);
