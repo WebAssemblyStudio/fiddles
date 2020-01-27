b4000319d8522572567b2b46b9ef684c386d7d00
WebAssemblyStudio <fiddles@webassembly.studio>
2020-01-27 13:14:28 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,20 @@
+export function init(_paramsPtr: usize, _messagePtr: usize): usize {
+  throw new Error("Not implemented");
+}
+
+export function handle(_paramsPtr: usize, _messagePtr: usize): usize {
+  throw new Error("Not implemented");
+}
+
+export function query(messagePtr: usize): usize {
+  // const msgJson = takeOwnership(messagePtr);
+  // log("JSON query request: " + Encoding.fromUtf8(msgJson));
+  // const msg = parse(msgJson).asObject();
+  // return wrapSuccessData(contract.query(msg));
+  return 0x0000;
+}
+
+// eslint-disable-next-line @typescript-eslint/camelcase
+export function cosmwasm_api_0_6(): i32 {
+  return 0x0603;
+}
