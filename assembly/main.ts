291b35da5b9c8e11ef0296cd9b8aa9a47b5bcf0a
WebAssemblyStudio <fiddles@webassembly.studio>
2020-02-02 23:24:36 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,27 @@
+let self: Object3D | null = null
+
+export class Object3D {
+	type: string = 'Object3D'
+
+	parent: Object3D | null = null
+	children: Object3D[] = []
+
+	constructor() {
+		// super()
+		self = this;
+		// this.rotation.onChange(() => {
+		//   self!.onRotationChange()
+		// })
+	}
+}
+
+export class Camera extends Object3D {
+  // type: string = 'Camera'
+	constructor() {
+		super()
+		this.type = 'Camera'
+	}
+}
+
+let camera = new Camera();
+assert(camera.type == 'Camera');
