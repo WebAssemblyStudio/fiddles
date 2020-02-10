6df9712f2be234b4ee7394221e4dbb6ee0bbf1c4
WebAssemblyStudio <fiddles@webassembly.studio>
2020-02-10 18:28:13 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const { sum, minus } = wasm_bindgen;
+
+
+function runApp() {
+  document.getElementById('result').value = sum(
+    document.getElementById('numA').value,
+    document.getElementById('numB').value
+  );
+  document.getElementById('result2').value = minus(
+    document.getElementById('numA').value,
+    document.getElementById('numB').value
+  );
+}
+
+// Load and instantiate the wasm file, and we specify the source of the wasm
+// file here. Once the returned promise is resolved we're ready to go and
+// use our imports.
+wasm_bindgen('../out/main_bg.wasm').then(runApp).catch(console.error);
