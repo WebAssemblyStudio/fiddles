98d4e4cb24f9fc52f0b9b61648e91bcb93312b00
WebAssemblyStudio <fiddles@webassembly.studio>
2020-02-15 21:01:17 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let buffer = null;
+
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  main: {
+    _print(ptr, len) {
+      const arr = new Uint8Array(buffer, ptr, len);
+      console.log('bytes: [' + arr + ']');
+      console.log('str: "' + [...arr].map(c => String.fromCharCode(c)).join('') + '"');
+    }
+  },
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  buffer = exports.memory.buffer;
+  document.getElementById("container").textContent = "Result: " + exports.main();
+}).catch(console.error);
