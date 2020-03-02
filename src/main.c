4b1667d1c0723aaabca3db4bf044576775cc6f6e
WebAssemblyStudio <fiddles@webassembly.studio>
2020-03-02 09:12:00 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+#include <stdio.h>
+extern int chain_method_exec(int, int);
+extern int (*chain_method_bind(int, int))(int);
+
+WASM_EXPORT
+int main() {
+    int (*f)(int) = chain_method_bind(1, 2);
+    return f(10);
+}
+
