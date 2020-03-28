828d8c0bc139ed82a26df84e2188212aebb09620
WebAssemblyStudio <fiddles@webassembly.studio>
2020-03-28 14:54:54 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,63 @@
+
+@inline
+function log2f_(x: f64): f64 {
+  const
+    log2e = 1.44269504088896340736,
+    c0 = 0.9999999993072205474,
+    c1 = 0.3333340818599626478,
+    c2 = 0.1998737838945025914,
+    c3 = 0.1496325726858180278;
+
+  let i = reinterpret<i64>(x);
+  let exponent = (i - 0x3FE6A09E667F3BCD) >> 52;
+  x = reinterpret<f64>(i - (exponent << 52));
+  x = (x - 1) / (x + 1);
+  let xx = x * x;
+  let y = x * (c0 + c1 * xx + (c2 + c3 * xx) * (xx * xx));
+  return (2 * log2e) * y + <f64>exponent;
+}
+
+@inline
+function exp2f_(x: f64): f64 {
+  if (x < -1022) return 0;
+  if (x >= 1024) return x * Infinity;
+  let n = nearest(x);
+  x -= n;
+  const 
+    c0 = 6.931471880289532425e-1,
+    c1 = 2.402265108421173406e-1,
+    c2 = 5.550357105498874537e-2,
+    c3 = 9.618030771171497658e-3,
+    c4 = 1.339086685300950937e-3,
+    c5 = 1.546973499989028719e-4;
+  let xx = x * x;
+  let y = 1 + x * (c0 + c1 * x + (c2 + c3 * x) * xx + (c4 + c5 * x) * (xx * xx));
+  return reinterpret<f64>(reinterpret<i64>(y) + (<i64>n << 52));
+}
+
+export function powf_new(x: f32, y: f32): f32 {
+  let sign: u32 = 0;
+  if (y == 0) return 1;
+  let sx = Mathf.signbit(x);
+  if (sx && nearest(y) == y) {
+    x = -x;
+    sign = u32(nearest(y / 2) != y / 2) << 31;
+  }
+  let z: f64;
+  if (x == 1) {
+    z = 1;
+  } else if (x == 0) {
+    z = Mathf.signbit(y) ? Infinity : 0;
+  } else if (abs(x) == Infinity) {
+    z = Mathf.signbit(y) ? 0 : Infinity;
+  } else if (sx) {
+    return NaN;
+  }
+  z = <f32>exp2f_(y as f64 * log2f_(x as f64));
+  let magnitude = reinterpret<u32>(z);
+  return reinterpret<f32>(magnitude | sign);
+}
+
+export function powf_old(x: f32, y: f32): f32 {
+  return Mathf.pow(x, y);
+}
