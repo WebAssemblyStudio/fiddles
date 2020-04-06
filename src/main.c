9923fee08afae4ed52781e1b5c7583d3220ba727
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-06 17:52:25 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,10 @@
+/* Inclusión de archivos */
+#include <stdio.h>
+
+/* Función principal */
+int main (int argc,char **argv)
+{
+   /* Impresión por pantalla y salida del programa*/
+   printf("Hola mundo\n");
+   return 0;
+}
