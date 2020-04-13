3f3b19779cae9a6b3501c85af05d06b4228d6441
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-13 14:25:13 +0000

    WebAssembly Studio fiddle

---
+++ src/ext.rs
@@ -0,0 +1,3 @@
+pub fn get_name() -> &'static str {
+  return "Bob";
+}
---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+mod ext;
+
+static mut NUM: i32 = 0;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+// Here we're exporting a function called `greet` which will display a greeting
+// for `name` through a dialog.
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+  // Make an integer to use for `coerce_static`:
+        let lifetime_num = 9;
+
+        // Coerce `NUM` to lifetime of `lifetime_num`:
+        let coerced_static = coerce_static(&lifetime_num);
+
+    alert(&format!("Hello, {}!", getName()));
+}
