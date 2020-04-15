bdaf07afd6039667c19d13cee362448d4e0e5e54
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-15 07:27:38 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+unsigned char raw[100];
+
+WASM_EXPORT
+unsigned char* getAddress() {
+	return &raw[0];
+}
+
+WASM_EXPORT
+int test() {
+
+	raw[0] = 3;
+	raw[1] = 4;
+	raw[2] = 5;
+
+	return raw[7];
+}
