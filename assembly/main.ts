883daa77acaffa2410cb38cb2067f9b68a33164b
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-15 14:54:11 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,25 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+
+//import { cl } from    
+
+//'nomain'
+ //import  {console, cl} from "./nomain.ts"
+
+
+
+
+
+namespace nomain {
+ 
+
+
+
+
+
+}
