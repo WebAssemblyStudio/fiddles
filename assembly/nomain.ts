e974b893cd63fa1f3ea1ab13221fafce67526d0c
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-15 14:49:25 +0000

    WebAssembly Studio fiddle

---
+++ assembly/nomain.ts
@@ -0,0 +1,24 @@
+namespace nomain{
+  class console {
+    //import { console } from 'console'
+
+
+// after save
+
+
+    public static log (e:String) {
+
+    }
+  
+
+
+  }
+  export class cl {
+    constructor(){
+    }
+    
+    public static stmt(){
+      console.log('df');  
+    }
+  }
+}
