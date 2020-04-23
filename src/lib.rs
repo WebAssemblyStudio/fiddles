d78be3d5e9e006afb275397807d4f77ac2188e03
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-23 20:51:36 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+mod ext;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_namespace = Math)]
+    fn log2(a: f64) -> f64;
+    #[wasm_bindgen(js_namespace = Math)]
+    fn sin(a: f64) -> f64;
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(a: &str);
+}
+
+macro_rules! println {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+const MAX_SIZE: u32 = 50;
+
+#[wasm_bindgen]
+pub fn run() -> *const u32 {
+    //println!("Math.log2(10.0) = {}", log2(10.0));
+    //println!("Math.sin(1) = {}", sin(ext::get_one()));
+    let mut state = [0u8, ..MAX_SIZE];
+
+    return state.as_ptr();
+}
