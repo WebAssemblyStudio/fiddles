9ef2cfb0494f3b3498cf58268217e72a5cddfc4a
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-27 17:15:48 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+#include <stdint.h>
+
+uint32_t gcd( uint32_t a, uint32_t b ) {
+  if ( b == 0 ) {
+    return a;
+  } else {
+    return gcd( b, a % b );
+  }
+}
+
+WASM_EXPORT
+int run( uint32_t x, uint32_t b ) {
+  return gcd( a, b );
+}
