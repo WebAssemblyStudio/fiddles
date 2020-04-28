1c2fa76357127cc0823a5e6c78c9a7e12d38c40c
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-28 00:18:59 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+#include <stdint.h>
+
+uint32_t fib( uint32_t n ) {
+  uint32_t a = 0;
+  uint32_t b = 1;
+  for ( uint32_t i = 0; i < n; i++ ) {
+    uint32_t t = a + b;
+    a = b;
+    b = t;
+  }
+  return a;
+}
+
+WASM_EXPORT
+uint32_t run( uint32_t a ) {
+  return fib( a );
+}
