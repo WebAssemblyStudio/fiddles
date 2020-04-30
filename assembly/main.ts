44ad596ecc251d54540bd22a31bd58ca264a4fc4
WebAssemblyStudio <fiddles@webassembly.studio>
2020-04-30 11:07:34 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,19 @@
+export const Float64Array_ID = idof<Float64Array>();
+
+export function sum(arr: Float64Array): f64 {
+  let sum = 0.0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+export function reduce(arr: Float64Array): f64 {
+  return arr.reduce((a, b) => a + b, 0.0);
+}
+
+export function emptyFunc(arr: Float64Array): f64 {
+  return 0.0;
+}
+
+
