5626a9ba9bd2f586c817288ba5ed3e9ddbb1b9f4
WebAssemblyStudio <fiddles@webassembly.studio>
2020-05-07 15:49:47 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,15 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+export function compass (f:Float32Array) : f32 {
+  // let len = f.length;
+  // let xx = new Uint16Array(1);
+  // xx[0] = 0x00FF;
+  //  let xx = new Float32Array(1); 
+  //  xx[0] =f;
+  return  f[0];
+}
