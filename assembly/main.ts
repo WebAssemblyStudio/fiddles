45fcb5aaee26962a329ee3971ca50fe2760c547c
WebAssemblyStudio <fiddles@webassembly.studio>
2020-05-24 14:19:17 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,12 @@
+export function add(x: i32, y: i32): u8 {
+  let buffer = new Uint8Array(1);
+  buffer[0] = 42;
+
+  //for (let index = 1; index < 2; index++) {
+  __retain(changetype<usize>(buffer)); // <-- add extra increment
+  const oldBuffer = buffer;
+  buffer = new Uint8Array(2);
+  buffer.set(oldBuffer);
+  buffer[1] = 1;
+  return buffer[0];
+}
