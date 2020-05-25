057080229436f572d0c80fbf00d0f733ecb56212
WebAssemblyStudio <fiddles@webassembly.studio>
2020-05-25 21:55:49 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,10 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  exports.test();
+}).catch(console.error);
