5cd25f2422bfbc826de5d041767837d418345856
WebAssemblyStudio <fiddles@webassembly.studio>
2020-05-26 10:39:15 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,38 @@
+export function squiggle(sequence: string, length: i32): Float64Array {
+  let x = [0.0]
+  let y = [0.0]
+  let yCoord = 0.0
+  let xCoord = 0.0
+
+  for (let i = 0; i < length; i++) {
+    let character = sequence.charAt(i)
+    x.push(xCoord + 0.5)
+    x.push(xCoord + 1.0)
+    xCoord += 1.0
+    if (character == "A") {
+      y.push(yCoord + 0.5)
+      y.push(yCoord)
+    } else if (character == "C") {
+      y.push(yCoord - 0.5)
+      y.push(yCoord)
+    } else if (character == "T") {
+      y.push(yCoord - 0.5)
+      y.push(yCoord - 1)
+      yCoord -= 1.0
+    } else if (character == "G") {
+      y.push(yCoord + 0.5)
+      y.push(yCoord + 1)
+      yCoord += 1.0
+    } else {
+      y.push(yCoord)
+      y.push(yCoord)
+    }
+  }
+
+  let result = new Float64Array((2 * length) + 2)
+  for (let i = 0; i < length; i++){
+    result[2 * i + 1] = x[i]
+    result[2 * i + 2] = y[i]
+  }
+  return result
+}
