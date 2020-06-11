4f2e1e5d2f8af348347406ff3196dc4f6097e1a2
WebAssemblyStudio <fiddles@webassembly.studio>
2020-06-11 09:53:39 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,28 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+
+  const dt = 0.01;
+  const steps = 20_000_000;
+
+  const exports = result.instance.exports;
+
+  console.log('[64-bit floats] n-body benchmark');
+  console.log(`steps: ${steps}, increment: ${dt}`);
+
+  // warmp
+  exports.init();
+  exports.bench(steps, dt);
+
+  // actual measurments
+  exports.init();
+  const startTime = performance.now();
+  exports.bench(steps, dt);
+  const deltaTime = performance.now() - startTime;
+
+  console.log('dt: ' + deltaTime + ' ms');
+}).catch(console.error);
