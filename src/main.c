ab27e7387b4d60427c95881e6f4051f0e78e873e
WebAssemblyStudio <fiddles@webassembly.studio>
2020-06-30 13:20:00 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+#include <emscripten.h>
+
+int main() {
+  EM_ASM(
+    alert('hello world!');
+    throw 'all done';
+  );
+  return 0;
+}
