4e9437914fde8ffc43b715657489b923a5dee0c4
WebAssemblyStudio <fiddles@webassembly.studio>
2020-07-17 14:18:54 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main() {
+  return 42;
+}
+
+WASM_EXPORT
+int fibo(int n){
+  if (n < 2)
+    return 1;
+  else
+    return fibo(n-1) + fibo(n-2);
+}
