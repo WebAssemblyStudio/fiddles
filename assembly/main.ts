7609be00c3d99e202ff6c241fc583b080dbf9a4c
WebAssemblyStudio <fiddles@webassembly.studio>
2020-07-21 07:01:50 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,4 @@
+
+export function getRand(): f64 {
+  return Math.random();
+}
---
+++ src/main.js
@@ -0,0 +1,16 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  main: {
+    sayHello() {
+      console.log("Hello from WebAssembly!");
+    }
+  },
+  env: {
+    seed: Date.now,
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  document.getElementById("container").textContent = "Result: " + exports.getRand();
+}).catch(console.error);
