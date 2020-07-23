721006a324d43bd94b4bc2222a277955c5694df2
WebAssemblyStudio <fiddles@webassembly.studio>
2020-07-23 18:46:00 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,84 @@
+export const Uint32Array_ID = idof<Uint32Array>();
+
+const stack = new StaticArray<u32>(100);
+
+export function floodFill(x: u32, y: u32, color: u32, width: u32, height: u32, data: Uint32Array): u32 {
+  const index = x + y * width;
+  const oldc32 = unchecked(data[index]);
+
+  let stackIndex: u32 = 0;
+  let count: u32 = 0;
+
+  unchecked(stack[stackIndex ++] = x);
+  unchecked(stack[stackIndex ++] = y);
+
+  while (stackIndex > 0) {
+    const cy = unchecked(stack[--stackIndex]);
+    const cx = unchecked(stack[--stackIndex]);
+
+    if (stackIndex < 0) {
+      break;
+    }
+
+    const i = cx + cy * width;
+
+    unchecked(data[i] = color);
+    count++;
+
+    // unroll
+    // for (let j = 0; j < 4; j ++) {
+
+    // j = 0;
+    let nx = cx + 0;
+    let ny = cy - 1;
+
+    if (nx >= 0 && ny >= 0 && nx < width && ny < height) {
+      const ni = (nx + ny * width);
+      
+      if (unchecked(data[ni]) === oldc32) {
+        unchecked(stack[stackIndex++] = nx);
+        unchecked(stack[stackIndex++] = ny);
+      }
+    }
+
+    // j = 1
+    nx = cx + 1;
+    ny = cy + 0;
+
+    if (nx >= 0 && ny >= 0 && nx < width && ny < height) {
+      const ni = nx + ny * width;
+      
+      if (unchecked(data[ni]) === oldc32) {
+        unchecked(stack[stackIndex++] = nx);
+        unchecked(stack[stackIndex++] = ny);
+      }
+    }
+
+    // j = 2
+    nx = cx + 0;
+    ny = cy + 1;
+
+    if (nx >= 0 && ny >= 0 && nx < width && ny < height) {
+      const ni = nx + ny * width;
+      
+      if (unchecked(data[ni]) === oldc32) {
+        unchecked(stack[stackIndex++] = nx);
+        unchecked(stack[stackIndex++] = ny);
+      }
+    }
+
+    // j = 3
+    nx = cx - 1;
+    ny = cy + 0;
+
+    if (nx >= 0 && ny >= 0 && nx < width && ny < height) {
+      const ni = nx + ny * width;
+      
+      if (unchecked(data[ni]) === oldc32) {
+        unchecked(stack[stackIndex++] = nx);
+        unchecked(stack[stackIndex++] = ny);
+      }
+    }
+  }
+  return count;
+}
