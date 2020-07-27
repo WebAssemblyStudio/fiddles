8f505da3435ae323933959937524da28dc4c97b5
WebAssemblyStudio <fiddles@webassembly.studio>
2020-07-27 13:20:27 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,14 @@
+
+
+// export function add(x: u32): u32 {
+//   return x / -2;
+// }
+
+export function t1(x: i32): i32 {
+  return x >>> 3;
+}
+
+export function t2(x: i32): i32 {
+  return x >> 3;
+}
+
