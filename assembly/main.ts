ff055e1c80ad713b4e875228c6cd605009b8ab05
WebAssemblyStudio <fiddles@webassembly.studio>
2020-09-16 15:32:30 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,15 @@
+
+var r1: i8 = 0;
+export var r2: i8 = 0;
+
+export function div1(x: i8, y: i8): void {
+  r1 = x / y;
+}
+
+export function div2(x: i8, y: i8): void {
+  r2 = x / y;
+}
+
+export function consumeGlobals(): i8 {
+  return r1 + r2;
+}
