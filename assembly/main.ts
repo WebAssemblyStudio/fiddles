8d69acc8e7bce731cf2aad72f0c35e1ab3984e43
WebAssemblyStudio <fiddles@webassembly.studio>
2020-09-21 16:57:15 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,9 @@
+
+
+export function test(c: u32, a: u8): u32 {
+  let factor: f32 = a ? 0xFF / <f32>a : 1;
+  let b = f32(c >> 24 & 0xFF) * factor;
+  let g = f32(c >> 16 & 0xFF) * factor;
+  let r = f32(c >>  8 & 0xFF) * factor;
+  return <u32><u8>a << 24 | <u32><u8>b << 16 | <u32><u8>g << 8 | <u32><u8>r << 8;
+}
