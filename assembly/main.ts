91e97499bde9b103f87a86ac851ffbf6b54f18f5
WebAssemblyStudio <fiddles@webassembly.studio>
2020-09-29 23:50:08 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,25 @@
+
+interface IFace {
+  run(): i32;
+}
+
+abstract class Base implements IFace {
+  abstract run(): i32;
+}
+
+class Instance extends Base {
+  run(): i32 {
+    return 88;
+  }
+}
+
+// var instance: IFace | null = null;
+
+export function run(): i32 {
+  let instance: IFace | null = new Instance();
+  let temp = instance;
+  if (temp !== null) {
+    return temp.run();
+  }
+  return 0;
+}
