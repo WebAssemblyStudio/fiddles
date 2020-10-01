1bb6a98a8ad5853d744e187b8109ebeda36b7e19
WebAssemblyStudio <fiddles@webassembly.studio>
2020-10-01 02:30:32 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn teste(name: &str) -> &str{
+    name
+}
+
