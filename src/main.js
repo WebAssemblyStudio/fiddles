8cea24db5348cc64bcfd19b813bcbd91c4659c20
WebAssemblyStudio <fiddles@webassembly.studio>
2020-10-15 08:30:07 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,7 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  document.getElementById("container").textContent = instance.exports.substract(10,10000);
+}).catch(console.error);
+
