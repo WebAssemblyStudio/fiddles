25a3c5655bdeba3517836d7b59f2ca7100b4298c
WebAssemblyStudio <fiddles@webassembly.studio>
2020-10-17 15:56:45 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+int data[256 * 256 * 256];
+int temp[256 * 256 * 256];
+
+WASM_EXPORT
+int* getDataPointer() {
+
+  return &data[0];
+
+}
+
+WASM_EXPORT
+int* getTempPointer() {
+
+  return &temp[0];
+
+}
+
+WASM_EXPORT
+int test(int *p, int shiftX, int shiftY, int shiftZ) {
+
+  int shift = shiftX + shiftY * 256 + shiftZ * 256 * 256;
+
+  for(int i = 0; i < 256 * 256 * 256; i++) {
+      
+    int src_i = i;
+		int dst_i = i + shift;
+
+    temp[dst_i] = p[src_i];
+
+  }
+  
+  memcpy(&data, &temp, 256 * 256 * 256 * 4);
+
+  return 0;
+
+}
---
+++ src/main.js
@@ -0,0 +1,36 @@
+let samples = [];
+
+
+WebAssembly.instantiateStreaming(fetch('../out/main.wasm'), {}).then(results => {
+
+  instance = results.instance;
+  instance.exports.memory.grow(1024 * 2);
+ 
+  let data_ptr = instance.exports.getDataPointer();
+  let data = new Uint32Array(instance.exports.memory.buffer, data_ptr, 256 * 256 * 256);
+
+  setInterval(() => {
+		
+    let timestamp = performance.now();
+  
+    data[0] = 1;
+    
+    instance.exports.test(data_ptr, 1, 0, 0);
+
+    samples.push(performance.now() - timestamp);
+
+		if (samples.length > 100) samples.shift();
+
+		let sum = samples.reduce((acc, val) => acc + val);
+
+    document.getElementById("container").innerHTML = `<pre>
+      forward_test ${(sum / samples.length / 10).toFixed(2)} ms
+      data[2] = ${data[1]}
+    </pre>`;
+
+  }, 1000);    
+
+  console.log(data[0]);
+
+  
+})
