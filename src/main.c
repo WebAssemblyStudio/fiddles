023076ddaec964ee394fc80220e8e191e595455c
WebAssemblyStudio <fiddles@webassembly.studio>
2020-10-18 11:15:34 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int shift_left(int *p, int shift) {
+
+  int shift_i = shift;
+
+  for(int z = 0; z < 256; z++) {
+  for(int y = 0; y < 256; y++) {
+  for(int x = 0; x < 256; x++) {
+
+    int i = x + y * 256  + z * 256 * 256;
+
+    p[i] = x < 256 - shift ? p[i + shift_i] : 0;
+
+  }}}
+      
+  return 1;
+
+}
