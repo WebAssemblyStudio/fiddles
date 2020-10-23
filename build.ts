aad84f2afa9874352311385dc4d3c0a2302cf519
WebAssemblyStudio <fiddles@webassembly.studio>
2020-10-23 10:57:48 +0000

    WebAssembly Studio fiddle

---
+++ build.ts
@@ -0,0 +1,14 @@
+import * as gulp from "gulp";
+import { Service, project } from "@wasm/studio-utils";
+
+gulp.task("build", async () => {
+  const data = await Service.compileFiles([
+    project.getFile("src/main.cpp")
+    ], "cpp", "wasm", "-std=c++17 -Wall -O2 -include-pch /include/DxLib/DxLib.O2.pch -I/include/DxLib -s MAIN_MODULE=1 -s ALLOW_MEMORY_GROWTH=1 -s FULL_ES2=1");
+  const outWasm = project.newFile("src/main.wasm", "wasm", true);
+  outWasm.setData(data["a.wasm"]);
+  const outJS = project.newFile("src/main.js", "javascript", true);
+  outJS.setData(data["wasm_bindgen.js"]);
+});
+
+gulp.task("default", ["build"], async () => {});
