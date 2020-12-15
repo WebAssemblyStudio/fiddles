09bf4bf8fd23b9143301380d2262a105344a441f
WebAssemblyStudio <fiddles@webassembly.studio>
2020-12-15 02:33:46 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,13 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+
+
+export class Point {
+  public x : number;
+  public y : number;
+}
