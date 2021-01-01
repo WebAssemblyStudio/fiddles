060be725f1ebdf269f4d5ef0ca179ce4e95c04d6
WebAssemblyStudio <fiddles@webassembly.studio>
2021-01-01 05:49:43 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const { log } = wasm_bindgen;
+
+function runApp() {
+  log('alert("test ok")');
+}
+
+// Load and instantiate the wasm file, and we specify the source of the wasm
+// file here. Once the returned promise is resolved we're ready to go and
+// use our imports.
+wasm_bindgen('../out/main_bg.wasm').then(runApp).catch(console.error);
