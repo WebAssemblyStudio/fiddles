543a33ec5a4e9f8901cb9d0f4f8acff6025ad599
WebAssemblyStudio <fiddles@webassembly.studio>
2021-01-06 17:47:25 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+#include <stdlib.h>
+#include <stdio.h>
+
+WASM_EXPORT
+int main() {
+  for(int i = 0; i < 5; i++) printf("ok\n");
+  return 42;
+}
