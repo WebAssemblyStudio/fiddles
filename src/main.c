c9ae57e2f3cb9e8709f22739ba63ffbd355cd5f5
WebAssemblyStudio <fiddles@webassembly.studio>
2021-03-01 22:06:02 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+#define WIDTH 100
+#define HEIGHT 100
+
+int data[WIDTH * HEIGHT];
+int red = (255 << 24) | 255;
+
+WASM_EXPORT
+int width() {
+  return WIDTH;
+}
+
+WASM_EXPORT
+int height() {
+  return HEIGHT;
+}
+
+WASM_EXPORT
+int* render() {
+   for (int y = 0; y < HEIGHT; y++) {
+     int yw = y * WIDTH;
+     for (int x = 0; x < WIDTH; x++) {
+       data[yw + x] = rand();
+     }
+   }
+   return &data[0];
+}
