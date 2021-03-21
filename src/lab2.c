2f288f8c28d611a1a77e1061a04f3c8ba63a30c9
WebAssemblyStudio <fiddles@webassembly.studio>
2021-03-21 10:01:36 +0000

    WebAssembly Studio fiddle

---
+++ src/lab2.c
@@ -0,0 +1,7 @@
+main float(float x ,float y ,float z) {
+scanf(float x ,float y ,float z)
+a = y + (x / y^(2) + abs(x^(2)/(y + x^(3)/3)))
+b = (1 + tanf^(2)z/2)
+printf("a=%f\n", a);
+printf("b=%f\n", b);
+}
