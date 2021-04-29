2a21b6f0913ce274b1de3225887a42760e913f64
WebAssemblyStudio <fiddles@webassembly.studio>
2021-04-29 16:06:39 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const WIDTH = 128, HEIGHT = 128, FPS = 10;
+
+const runWasm = async () => {
+  const importObject = {
+    env: {
+      seed: Date.now,
+      abort: () => console.log('aborting!')
+    }
+  };
+  const module = await WebAssembly.instantiateStreaming(fetch('../out/main.wasm'), importObject);
+  const exports = module.instance.exports;
+
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  exports.init(WIDTH, HEIGHT);
+
+  const memoryBuffer = exports.memory.buffer;
+  const memoryArray = new Uint8ClampedArray(memoryBuffer)
+  
+  const imageData = ctx.createImageData(WIDTH, HEIGHT);
+
+  const step = () => {
+    exports.update();
+  
+    imageData.data.set(memoryArray.slice(0, WIDTH * HEIGHT * 4));
+    ctx.putImageData(imageData, 0, 0);
+  
+    setTimeout(step, 1000 / FPS);
+  };
+  step();
+
+}
+runWasm();
