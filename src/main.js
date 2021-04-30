c78af33ea7117aa1c5010ed47286fe959e5e4f79
WebAssemblyStudio <fiddles@webassembly.studio>
2021-04-30 20:53:04 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,21 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const container = document.getElementById("container");
+  container.textContent = '';
+
+  const { batchPush, batchPushNew } = result.instance.exports;
+  const N = 1000000;
+
+  let t = performance.now();
+  // batchPush(N); // FF88 = 61557 ms, Chrome90 = 69552 ms
+  batchPushNew(N); // FF88 = 9 ms, Chrome90 = 9 ms
+  t = performance.now() - t;
+
+  container.textContent += `Array#push(${N}): ${t} ms\n`;
+
+}).catch(console.error);
