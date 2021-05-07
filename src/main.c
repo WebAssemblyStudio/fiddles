6f9db8ac45798675e4a137b9ca68e50cdefef0af
WebAssemblyStudio <fiddles@webassembly.studio>
2021-05-07 08:35:01 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+#include <time.h>
+WASM_EXPORT
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N (10 * 1000 * 1000)
+
+WASM_EXPORT
+int is_prime(int x)
+{
+  int primes[N] = {2};
+  size_t primes_count = 1;
+    for (size_t i = 0; primes[i] * primes[i] <= x; ++i) {
+        if (x % primes[i] == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
