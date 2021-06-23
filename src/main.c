2e6c2c130c83a57ed6b2be42c7b891f949ff75f9
WebAssemblyStudio <fiddles@webassembly.studio>
2021-06-23 21:59:31 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+const unsigned WIDTH = 500;
+const unsigned HEIGHT = 500;
+WASM_EXPORT
+unsigned int SCREEN_BUFFER[WIDTH * HEIGHT];
+
+WASM_EXPORT
+unsigned int getScreenWidth(){
+  return WIDTH;
+}
+
+WASM_EXPORT
+unsigned int getScreenHeight(){
+  return HEIGHT;
+}
