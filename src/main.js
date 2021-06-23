200318de7ad1d4e620078ef9242f4b4f99ef0b09
WebAssemblyStudio <fiddles@webassembly.studio>
2021-06-23 22:00:33 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,8 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  
+  
+
+}).catch(console.error);
