dd4565984bd836180a39635b71c139d115c91f2d
WebAssemblyStudio <fiddles@webassembly.studio>
2021-06-23 22:20:18 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,42 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+const unsigned WIDTH = 300;
+const unsigned HEIGHT = 300;
+
+int ScreenOffsetX = 0;
+int ScreenOffsetY = 0;
+
+unsigned int drawingColor = 0xffffffff;
+
+WASM_EXPORT
+unsigned int SCREEN_BUFFER[WIDTH * HEIGHT];
+
+WASM_EXPORT
+unsigned int getScreenWidth(){
+  return WIDTH;
+}
+
+WASM_EXPORT
+unsigned int getScreenHeight(){
+  return HEIGHT;
+}
+
+void putPixel(int x, int y){
+  if(!(x-ScreenOffsetX > WIDTH || x-ScreenOffsetX < 0 || y-ScreenOffsetY > HEIGHT || y-ScreenOffsetY < 0)){
+    SCREEN_BUFFER[(y-ScreenOffsetY)*WIDTH+x-ScreenOffsetX] = drawingColor;
+  }
+}
+
+void rect(int x, int y, int width, int height){
+  for(int i = 0; i < width; i++){
+    for(int j = 0; j < height; j++){
+      putPixel(x+i, y+j);
+    }
+  }
+}
+
+WASM_EXPORT
+void update(){
+  drawingColor = 0xff000000;
+  rect(0, 0, WIDTH, HEIGHT);
+}
---
+++ src/main.js
@@ -0,0 +1,32 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  
+  let width = instance.exports.getScreenWidth();
+  let height = instance.exports.getScreenHeight();
+
+  let data = new ImageData(
+    new Uint8ClampedArray(
+      instance.exports.memory.buffer,
+      instance.exports.SCREEN_BUFFER.value,
+      4 * width * height
+    ),
+    height
+  );
+
+  let cvs = document.getElementById("cvs");
+  cvs.width = width;
+  cvs.height = height;
+  let ctx = cvs.getContext("2d");
+
+  (function update(){
+
+    instance.exports.update();
+
+    ctx.putImageData(data, 0, 0);
+
+    requestAnimationFrame(update);
+  })();
+
+}).catch(console.error);
