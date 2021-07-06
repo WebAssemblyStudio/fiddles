7cfcf34af0167ae4566cfd81cc779fc7bbe43a05
WebAssemblyStudio <fiddles@webassembly.studio>
2021-07-06 01:03:26 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,13 @@
+//fetch('../out/main.wasm').then(response =>
+//  response.arrayBuffer()
+//).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+//  instance = results.instance;
+//  document.getElementById("container").textContent = instance.exports.main();
+//}).catch(console.error);
+
+(async() => {
+  const codePromise = fetch('../out/main.wasm')
+  const module = await WebAssembly.instantiateStreaming(codePromise)
+  console.log(module)
+
+})
