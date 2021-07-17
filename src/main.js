8a72e926164db9b1eab6289012522b437b5953e5
WebAssemblyStudio <fiddles@webassembly.studio>
2021-07-17 17:48:15 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// Load WebAssembly module
+window.results = await WebAssembly.instantiateStreaming( fetch('../out/main.wasm'), { js: { global: 10 } } ).catch(console.error)
+window.instance = results.instance
+window.exports = instance.exports
+
+window.container = document.querySelector("#result")
+
+exports.setGlobal(33)
+window.container.innerHTML = exports.getGlobal()
+
+//window.container.innerHTML = exports.add(10, 1)
+
+
+
+
+// Put download link of the wasm file
+const wasmFile = document.querySelector('#wasmFile')
+const wasmBlob = await (await fetch(wasmFile.href)).blob()
+wasmFile.href = URL.createObjectURL(wasmBlob)
+
+
+
+
+// Load StyleSheet
+const myStyleSheet = new CSSStyleSheet
+await myStyleSheet.replace( await (await fetch('main.css')).text() )
+
+document.adoptedStyleSheets = [myStyleSheet]
