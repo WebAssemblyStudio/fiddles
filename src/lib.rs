a552be3d0e0d6fd24b350c47ac31e629a75d9a9c
WebAssemblyStudio <fiddles@webassembly.studio>
2021-09-02 17:22:48 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+// Here we're exporting a function called `greet` which will display a greeting
+// for `name` through a dialog.
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    let result: u8 = add(2,2);
+    alert(&format!("Hello, {}! 2+2={}", name,result));
+    println!("Hello");
+}
+
+#[wasm_bindgen]
+pub fn add(num1: u8, num2: u8) -> u8 {
+  num1 + num2
+}
