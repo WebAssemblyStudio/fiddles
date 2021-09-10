7adc28b6602a9c6cc3d0e133e24a36245640f7f9
WebAssemblyStudio <fiddles@webassembly.studio>
2021-09-10 10:45:46 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,11 @@
+
+
+export function saw(x: f32, amp: f32, freq: f32): f32 {
+  const hpi: f32 = Mathf.PI / 2.0;
+  const norm: f32 = 2.0 / Mathf.PI;
+  return amp * (norm * abs(Mathf.PI - abs(freq * x - hpi) % (2.0 * Mathf.PI)) - 1.0);
+}
+
+export function sin(x: f32, amp: f32, freq: f32): f32 {
+  return amp * Mathf.sin(x * freq);
+}
