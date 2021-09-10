a9f75abd8956e41352b7940bedef1a5488ba930e
WebAssemblyStudio <fiddles@webassembly.studio>
2021-09-10 11:05:39 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,17 @@
+
+@inline
+function fastMod(x: f32, y: f32): f32 {
+  return x - y * Mathf.floor(x / y);
+}
+
+// 413 bytes
+export function saw(x: f32, amp: f32, freq: f32): f32 {
+  const hpi: f32 = Mathf.PI / 2.0;
+  const norm: f32 = 2.0 / Mathf.PI;
+  return amp * (norm * abs(Mathf.PI - fastMod(abs(freq * x - hpi), 2.0 * Mathf.PI)) - 1.0);
+}
+
+// 1.2 kb
+// export function sin(x: f32, amp: f32, freq: f32): f32 {
+//   return amp * Mathf.sin(x * freq);
+// }
