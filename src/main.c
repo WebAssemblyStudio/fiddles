01436f4a5d89906d25bed810e28600d1f878d139
WebAssemblyStudio <fiddles@webassembly.studio>
2021-09-17 06:27:48 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+WASM_EXPORT
+int main() {
+  unsigned long int i;
+  unsigned long int max;
+  for(i=0; i<18446744073709551615; i++){
+    //
+  }
+  return 0;
+}
