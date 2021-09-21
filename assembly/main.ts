530157b45b176778c8763186f32188b4f5160636
WebAssemblyStudio <fiddles@webassembly.studio>
2021-09-21 00:19:14 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,14 @@
+declare function sayHello(): void;
+
+sayHello();
+
+for(let i = 0; i < 5; i++)
+{
+  //console.log(i.toString());
+}
+
+
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
