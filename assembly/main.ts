4165c85da81ff0e94653787230fdacf80a505783
WebAssemblyStudio <fiddles@webassembly.studio>
2021-09-22 18:52:35 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,16 @@
+function freeFunction<T>(value: T): T {
+  return value;
+}
+
+// function freeFunctionI64(value: i64): i64 {
+//   return freeFunction(value);
+// }
+
+function bad<T>(value: T, func: (value: T) => T): T {
+  return func(value);
+}
+
+export function test(): i64 {
+  return bad<i64>(123, freeFunction);
+  // return bad<i64>(123, freeFunctionI64);
+}
