f845d5116dd0cfb6d61d644f260efe4e511da14f
WebAssemblyStudio <fiddles@webassembly.studio>
2021-10-10 17:32:40 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,83 @@
+let x = '../out/main.wasm';
+
+let instance = null;
+let memoryStates = new WeakMap();
+
+function syscall(instance, n, args) {
+  switch (n) {
+    default:
+      // console.log("Syscall " + n + " NYI.");
+      break;
+    case /* brk */ 45: return 0;
+    case /* writev */ 146:
+      return instance.exports.writev_c(args[0], args[1], args[2]);
+    case /* mmap2 */ 192:
+      debugger;
+      const memory = instance.exports.memory;
+      let memoryState = memoryStates.get(instance);
+      const requested = args[1];
+      if (!memoryState) {
+        memoryState = {
+          object: memory,
+          currentPosition: memory.buffer.byteLength,
+        };
+        memoryStates.set(instance, memoryState);
+      }
+      let cur = memoryState.currentPosition;
+      if (cur + requested > memory.buffer.byteLength) {
+        const need = Math.ceil((cur + requested - memory.buffer.byteLength) / 65536);
+        memory.grow(need);
+      }
+      memoryState.currentPosition += requested;
+      return cur;
+  }
+}
+function Vector2(x,y)
+{
+  this.x = x;
+  this.y = y;
+  this.ToPointer = function(buffer) {
+    var buff = new Float32Array(buffer,0,8);
+    buff.set([x,y]);
+    console.log()
+    return buff;
+  }
+}
+let s = "";
+fetch(x).then(response =>
+  response.arrayBuffer()
+).then(bytes =>
+  WebAssembly.instantiate(bytes, {
+    env: {
+      __syscall0: function __syscall0(n) { return syscall(instance, n, []); },
+      __syscall1: function __syscall1(n, a) { return syscall(instance, n, [a]); },
+      __syscall2: function __syscall2(n, a, b) { return syscall(instance, n, [a, b]); },
+      __syscall3: function __syscall3(n, a, b, c) { return syscall(instance, n, [a, b, c]); },
+      __syscall4: function __syscall4(n, a, b, c, d) { return syscall(instance, n, [a, b, c, d]); },
+      __syscall5: function __syscall5(n, a, b, c, d, e) { return syscall(instance, n, [a, b, c, d, e]); },
+      __syscall6: function __syscall6(n, a, b, c, d, e, f) { return syscall(instance, n, [a, b, c, d, e, f]); },
+      putc_js: function (c) {
+        c = String.fromCharCode(c);
+        if (c == "\n") {
+          console.log(s);
+          s = "";
+        } else {
+          s += c;
+        }
+      }
+    }
+  })
+).then(results => {
+
+  instance = results.instance;
+  var {main,memory,GetArea} = instance.exports;
+  var vector2 = new Vector2(3,4);
+  var buff = vector2.ToPointer(memory.buffer);
+  var result = GetArea(buff.byteOffset)
+  const memory2 = new WebAssembly.Memory({ initial: 2 });
+  console.log(result);
+  var c = new Float32Array(memory2.buffer,0,8);
+  c.set([33,33])
+  console.log(buff)
+  document.getElementById("container").textContent = instance.exports.main();
+}).catch(console.error);
