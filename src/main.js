e75e7d7d68ac56039b2369a3acd638938c460628
WebAssemblyStudio <fiddles@webassembly.studio>
2021-10-13 04:35:49 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// This is our recommended way of loading WebAssembly.
+// https://developers.google.com/web/updates/2018/04/loading-wasm
+(async () => {
+  const fetchPromise = fetch('../out/fibonacci.wasm');
+  const { instance } = await WebAssembly.instantiateStreaming(fetchPromise);
+  const result = instance.exports.fibonacci(42);
+  document.querySelector('main').textContent = `The 42nd Fibonacci number is ${ result }.`;
+})();
