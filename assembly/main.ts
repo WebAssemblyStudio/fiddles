56996d35618ec27fe5f241bf0c3bfb743153c846
WebAssemblyStudio <fiddles@webassembly.studio>
2021-10-18 21:12:30 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,17 @@
+declare function sayHello(): void;
+
+
+@unmanaged
+export class Vec3 {
+  x: f32;
+  y: f32;
+  z: f32;
+}
+
+
+sayHello();
+
+export function add(x: i32, y: i32): i32 {
+  return x + y;
+}
+
---
+++ src/main.js
@@ -0,0 +1,17 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  main: {
+    sayHello() {
+      console.log("Hello from WebAssembly!");
+    }
+  },
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  
+  const vec3 = new result.Vec3(1, 2, 3)
+  document.getElementById("container").textContent = "Result: " + vec3.x;
+}).catch(console.error);
