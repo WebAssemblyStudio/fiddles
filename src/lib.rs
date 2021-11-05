935ec43e6986dbbd2ac9620cfba456989a3778d9
WebAssemblyStudio <fiddles@webassembly.studio>
2021-11-05 00:49:23 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+use std::io;
+
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
