26f5c9e9decf8fe34ad6a44e153218284f7e6435
WebAssemblyStudio <fiddles@webassembly.studio>
2021-12-06 13:02:55 +0000

    WebAssembly Studio fiddle

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+// Current prelude for using `wasm_bindgen`, and this'll get smaller over time!
+#![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+// Here we're importing the `alert` function from the browser, using
+// `#[wasm_bindgen]` to generate correct wrappers.
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+// Here we're exporting a function called `greet` which will display a greeting
+// for `name` through a dialog.
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    use wasm_bindgen::__rt::core::mem::size_of_val;
+
+    alert(&format!("Hello, {}!", name));
+    let input = "5,1,1,5,4,2,1,2,1,2,2,1,1,1,4,2,2,4,1,1,1,1,1,4,1,1,1,1,1,5,3,1,4,1,1,1,1,1,4,1,5,1,1,1,4,1,2,2,3,1,5,1,1,5,1,1,5,4,1,1,1,4,3,1,1,1,3,1,5,5,1,1,1,1,5,3,2,1,2,3,1,5,1,1,4,1,1,2,1,5,1,1,1,1,5,4,5,1,3,1,3,3,5,5,1,3,1,5,3,1,1,4,2,3,3,1,2,4,1,1,1,1,1,1,1,2,1,1,4,1,3,2,5,2,1,1,1,4,2,1,1,1,4,2,4,1,1,1,1,4,1,3,5,5,1,2,1,3,1,1,4,1,1,1,1,2,1,1,4,2,3,1,1,1,1,1,1,1,4,5,1,1,3,1,1,2,1,1,1,5,1,1,1,1,1,3,2,1,2,4,5,1,5,4,1,1,3,1,1,5,5,1,3,1,1,1,1,4,4,2,1,2,1,1,5,1,1,4,5,1,1,1,1,1,1,1,1,1,1,3,1,1,1,1,1,4,2,1,1,1,2,5,1,4,1,1,1,4,1,1,5,4,4,3,1,1,4,5,1,1,3,5,3,1,2,5,3,4,1,3,5,4,1,3,1,5,1,4,1,1,4,2,1,1,1,3,2,1,1,4";
+    let mut fishs: Vec<u8> = input.split(",").map(|n| n.parse().unwrap()).collect();
+    
+    const DAYS: u32 = 256;
+    
+    for current_day in 0..DAYS {
+        let mut fishs_to_add = 0;
+        
+        for fish in &mut fishs {
+            if fish == &0u8 {
+                fishs_to_add += 1;
+                *fish = 6;
+            } else {
+                *fish -= 1;
+            }
+        }
+        
+        let fishs_to_add = vec![8u8; fishs_to_add];
+        fishs.extend(&fishs_to_add);
+
+        log(&format!("Size of fishs: {}", size_of_val(&*fishs)));
+        log(&format!("After {} day(s): -> {}", current_day+1/*, fishs*/, fishs.len()));
+    }
+}
