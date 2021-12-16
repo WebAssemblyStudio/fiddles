14c15b0e4f0f5893fc9385d15cf7a6b69e6458b7
WebAssemblyStudio <fiddles@webassembly.studio>
2021-12-16 11:51:34 +0000

    WebAssembly Studio fiddle

---
+++ assembly/main.ts
@@ -0,0 +1,10 @@
+declare function sayHello(): void;
+
+sayHello();
+
+export function add(a: i32, b: i32): i32 {
+  return a + b;
+}
+export function mult(a: i32, b: i32): i32 {
+  return a * b;
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+WebAssembly.instantiateStreaming(fetch("../out/main.wasm"), {
+  main: {
+    sayHello() {
+      console.log("Hello from WebAssembly!");
+    }
+  },
+  env: {
+    abort(_msg, _file, line, column) {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+}).then(result => {
+  const exports = result.instance.exports;
+  document.getElementById("container").textContent = "Result: " + exports.add(19, 23);
+}).then(result => {
+  const exports = result.instance.exports;
+  document.getElementById("container").textContent = "Result: " + exports.mult(2, 2);
+}).catch(console.error);
