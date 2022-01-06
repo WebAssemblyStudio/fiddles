c7c01c6462ceaa815f1dd25b97c2492a5a96df87
WebAssemblyStudio <fiddles@webassembly.studio>
2022-01-06 14:56:58 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,6 @@
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  instance = results.instance;
+  document.getElementById("container").textContent = instance.exports.add(BigInt(888888888), BigInt(888888889));
+}).catch(console.error);
