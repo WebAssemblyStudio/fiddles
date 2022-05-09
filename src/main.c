5f86fb77dcccf8bc4658c12f7159b0fa4e473957
WebAssemblyStudio <fiddles@webassembly.studio>
2022-05-09 19:18:51 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+int SIZE = 256;
+char BUFFER[256];
+
+WASM_EXPORT
+char* get_offset() {
+  return &BUFFER[0];
+  // NB: les pointeurs sont vus par Javascript comme des entiers
+}
+
+WASM_EXPORT
+int get_size() {
+  return SIZE;
+}
+
+// met le contenu de BUFFER en majuscules
+WASM_EXPORT
+void upper() {
+  for (int i=0; i<SIZE; i++) {
+    if ('a' <= BUFFER[i] && BUFFER[i] <= 'z') {
+      BUFFER[i] -= 32;
+    }
+  }
+}
