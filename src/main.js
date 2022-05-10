302350a93285a91992df5ed7b58e75bcf304977d
WebAssemblyStudio <fiddles@webassembly.studio>
2022-05-10 15:27:28 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,27 @@
+var c = document.querySelector("canvas");
+var ctx = c.getContext('2d');
+
+WebAssembly.instantiateStreaming(
+  fetch('../out/main.wasm'),
+  {},
+)
+.then((result) => {
+  let {memory, get_size, get_offset, fill, deplacementFourmi} = result.instance.exports;
+  let size = get_size();
+  c.width = size;
+  c.height = size;
+  let array = new Uint8ClampedArray(
+    memory.buffer,
+    get_offset(),
+    size*size*4,
+  );
+  imageData = new ImageData(array, size, size);
+  fill();
+  while(true){
+    deplacementFourmi()
+  }
+  ctx.putImageData(imageData, 0, 0);
+})
+.catch((err) => {
+  console.log("Error" + err);
+});
