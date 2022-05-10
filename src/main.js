194abd2f4623bb014b230faff88636170d3c2489
WebAssemblyStudio <fiddles@webassembly.studio>
2022-05-10 16:22:25 +0000

    WebAssembly Studio fiddle

---
+++ src/main.js
@@ -0,0 +1,45 @@
+var c = document.querySelector("canvas");
+var ctx = c.getContext('2d');
+
+
+
+fetch('../out/main.wasm').then(response =>
+  response.arrayBuffer()
+).then(bytes => WebAssembly.instantiate(bytes)).then(results => {
+  let {memory, get_size, get_offset, initialize, move} = results.instance.exports;
+  let size = get_size();
+  c.width = size;
+  c.height = size;
+  let array = new Uint8ClampedArray(
+    memory.buffer,
+    get_offset(),
+    size*size*4,
+  );
+  initialize();
+  imageData = new ImageData(array, size, size);
+  ctx.putImageData(imageData, 0, 0);
+
+  let simulation;
+
+  const start = () => {
+    clearInterval(simulation);
+    simulation = setInterval( () => {
+      imageData = new ImageData(array, size, size);
+      move();
+      ctx.putImageData(imageData, 0, 0);
+    },0.5)
+  }
+
+  const stop = () => {
+    clearInterval(simulation);
+  } 
+
+  
+  document.getElementById('start-btn').onclick = start;
+  document.getElementById('close-btn').onclick = stop;
+  
+
+
+
+  //document.getElementById("container").textContent = instance.exports.main();
+}).catch(console.error);
