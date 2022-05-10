959b8c862fdf492094d5be2fee4397100f7a50dc
WebAssemblyStudio <fiddles@webassembly.studio>
2022-05-10 14:22:10 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+const int SIZE = 256;
+unsigned char BUFFER[SIZE][SIZE][4];
+const int R = 0;
+const int G = 1;
+const int B = 2;
+const int A = 3;
+
+WASM_EXPORT
+void* get_offset() {
+  return &BUFFER;
+}
+
+WASM_EXPORT
+int get_size() {
+  return SIZE;
+}
+
+WASM_EXPORT
+// Génère une image dans BUFFER
+void fill() {
+  for (int y=0; y<SIZE; y++) {
+    for (int x=0; x<SIZE; x++) {
+      BUFFER[y][x][R] = x % 256;
+      BUFFER[y][x][G] = x % 256;
+      BUFFER[y][x][B] = x % 256;
+      BUFFER[y][x][A] = 255;
+    }
+  }
+}
