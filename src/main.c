c3910017035989d5eeb171b31246dab8738b261c
WebAssemblyStudio <fiddles@webassembly.studio>
2022-05-10 14:52:47 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,74 @@
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+const int SIZE = 256;
+unsigned char BUFFER[SIZE][SIZE][4];
+const int R = 0;
+const int G = 1;
+const int B = 2;
+const int A = 3;
+
+const int NORD = 0;
+const int EST = 1;
+const int SUD = 2;
+const int OUEST = 3;
+const int direction = NORD;
+const int xPos = SIZE / 2;
+const int yPos = SIZE / 2;
+
+
+WASM_EXPORT
+void* get_offset() {
+  return &BUFFER;
+}
+
+WASM_EXPORT
+int get_size() {
+  return SIZE;
+}
+
+WASM_EXPORT
+// Génère une image dans BUFFER
+void fill() {
+  for (int y=0; y<SIZE; y++) {
+    for (int x=0; x<SIZE; x++) {
+      BUFFER[y][x][R] = 255;
+      BUFFER[y][x][G] = 255;
+      BUFFER[y][x][B] = 255;
+      BUFFER[y][x][A] = 255;
+    }
+  }
+}
+
+void colorBlack(x, y) {
+  BUFFER[y][x][R] = 0;
+  BUFFER[y][x][G] = 0;
+  BUFFER[y][x][B] = 0;
+}
+
+void colorWhite(x, y) {
+  BUFFER[y][x][R] = 255;
+  BUFFER[y][x][G] = 255;
+  BUFFER[y][x][B] = 255;
+}
+
+void turnLeft() {
+  direction = (direction - 1) % 4
+}
+
+void turnRight() {
+  direction = (direction + 1) % 4
+}
+
+WASM_EXPORT
+void avancer() {
+  int color = BUFFER[yPos][xPos][R]
+  if (color == 255) {
+    turnLeft();
+    colorBlack();
+  }
+  else if (color == 0) {
+    turnRight();
+    colorWhite();
+  }
+}
+
---
+++ src/main.js
@@ -0,0 +1,28 @@
+var c = document.querySelector("canvas");
+var ctx = c.getContext('2d');
+
+WebAssembly.instantiateStreaming(
+  fetch('../out/main.wasm'),
+  {},
+)
+.then((result) => {
+  let {memory, get_size, get_offset, fill} = result.instance.exports;
+  let size = get_size();
+  c.width = size;
+  c.height = size;
+  let array = new Uint8ClampedArray(
+    memory.buffer,
+    get_offset(),
+    size*size*4,
+  );
+  imageData = new ImageData(array, size, size);
+  fill();
+  drawImage();
+})
+.catch((err) => {
+  console.log("Error" + err);
+});
+
+function drawImage() {
+  ctx.putImageData(imageData, 0, 0);
+}
