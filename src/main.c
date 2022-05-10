09d5c4bcbca273e8c6c19d1e6de403292a93be3e
WebAssemblyStudio <fiddles@webassembly.studio>
2022-05-10 15:13:23 +0000

    WebAssembly Studio fiddle

---
+++ src/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#define WASM_EXPORT __attribute__((visibility("default")))
+
+const int SIZE = 256;
+unsigned char BUFFER[SIZE][SIZE][4];
+const int R = 0;
+const int G = 1;
+const int B = 2;
+const int A = 3;
+int orientationFourmi = 0; //0 haut, 1 bas, 2 droite, 3 gauche
+
+WASM_EXPORT
+void* get_offset() {
+  return &BUFFER;
+}
+
+WASM_EXPORT
+int get_size() {
+  return SIZE;
+}
+
+
+
+WASM_EXPORT
+// Génère une image dans BUFFER
+void fill() {
+  for (int y=0; y<SIZE; y++) {
+    for (int x=0; x<SIZE; x++) {
+      BUFFER[y][x][R] = 0;
+      BUFFER[y][x][G] = 0;
+      BUFFER[y][x][B] = 0;
+      BUFFER[y][x][A] = 255;
+    }
+  }
+
+  //int posX = rand() % SIZE;
+  //int posY = rand() % SIZE;
+
+  int posX = SIZE/2;
+  int posY = SIZE/2;
+
+  BUFFER[posX][posY][R] = 255;
+  BUFFER[posX][posY][G] = 255;
+  BUFFER[posX][posY][B] = 255;
+
+}
+
+void deplacementFourmi(orientationFourmi,posX,posY){
+  switch orientationFourmi{
+    case 0: //haut
+          posX+=1;
+          orientationFourmi = 2;
+    case 1: //bas
+          posX-=1;
+          orientationFourmi = 3;
+    case 2: // droite
+          posY-=1;
+          orientationFourmi = 1;
+    case 3: // gauche
+          posY+=1;
+          orientationFourmi = 0
+  }
+}
